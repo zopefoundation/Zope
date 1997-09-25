9493bfeddd849ed3bbb0c401b1de144a6c9bea9e
Jim Fulton <jim@zope.com>
1997-09-25 18:57:45 +0000

    *** empty log message ***

---
+++ lib/python/Shared/DC/__init__.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+############################################################################## 
+#
+#     Copyright 
+#
+#       Copyright 1996 Digital Creations, L.C., 910 Princess Anne
+#       Street, Suite 300, Fredericksburg, Virginia 22401 U.S.A. All
+#       rights reserved.
+#
+############################################################################## 
+__doc__='''$Id: __init__.py,v 1.1 1997/09/25 18:57:45 jim Exp $'''
+__version__='$Revision: 1.1 $'[11:-2]
+
+
+import sys, string, os
+
+__path__=__.__path__
+
+for o in 's', 'r':
+    try: 
+	s=os.popen('uname -s').read()[:-1]
+	r=os.popen('uname -r').read()[:-1]
+	if r and s:
+	    p=(string.lower(s)+r)
+	    if p: __path__.append("%s/bin-%s/%s" % (
+		__path__[0], string.split(sys.version)[0], p))
+    except: pass
+
+__path__.append("%s/bin-%s/%s" % (
+    __path__[0], string.split(sys.version)[0], sys.platform))
+    
+############################################################################## 
+#
+# $Log: __init__.py,v $
+# Revision 1.1  1997/09/25 18:57:45  jim
+# *** empty log message ***
+#
+#
