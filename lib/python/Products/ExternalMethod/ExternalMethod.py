8707f4715118d79d44ceeab77ac54d47fc68b325
Jim Fulton <jim@zope.com>
1997-10-09 18:26:50 +0000

    first working version

--- lib/python/Products/ExternalMethod/ExternalMethod.py
+++ lib/python/Products/ExternalMethod/ExternalMethod.py
@@ -6,33 +6,61 @@ domain-specific customization of web environments.
 """
 
 from Acquisition import Implicit
+from Globals import Persistent, HTMLFile, MessageDialog
+import OFS.SimpleItem
+from string import split, join
+import AccessControl.Role
 	    
-brain_dir=SOFTWARE_HOME+'/Extensions'    
+braindir=SOFTWARE_HOME+'/Extensions'    
 modules={}
 
-def add(self, id, title, roles, external_name, REQUEST=None):
+addForm=HTMLFile('ExternalMethod/methodAdd')
+
+def add(self, id, title, external_name, acl_type='A',acl_roles=[], REQUEST=None):
     """Add an external method to a folder"""
     names=split(external_name,'.')
     module, function = join(names[:-1],'.'), names[-1]
-    i=ExternalMethod(id,title,roles,module,function)
-    self._setObject(name,i)
+    i=ExternalMethod(id,title,module,function)
+    i._setRoles(acl_type,acl_roles)
+    self._setObject(id,i)
     return self.manage_main(self,REQUEST)
 
-class ExternalMethod:
+class ExternalMethod(OFS.SimpleItem.Item, Persistent, AccessControl.Role.RoleManager):
     """An external method is a web-callable function that encapsulates
     an external function."""
 
     meta_type='External Method'
+    icon='ExternalMethod/function.gif'
+    func_defaults=()
+    func_code=None
 
-    def __init__(self, id='', title='', roles='', module='', function=''):
+    def __init__(self, id='', title='', module='', function=''):
 	if id:
 	    self.id=id
 	    self.title=title
-	    self.roles=roles
 	    self._module=module
 	    self._function=function
 	    self.getFunction()
         self._p_atime=1
+
+    manage=HTMLFile('ExternalMethod/methodEdit')
+    def manage_edit(self, title, external_name, acl_type='A', acl_roles=[],
+		    REQUEST=None):
+	"Change the external method"
+	self.title=title
+	names=split(external_name,'.')
+	module, function = join(names[:-1],'.'), names[-1]
+	self._module=module
+	self._function=function
+	self.getFunction()
+	self._setRoles(acl_type,acl_roles)
+	if REQUEST: return MessageDialog(
+	    title  ='Changed %s' % self.id,
+	    message='%s has been updated' % self.id,
+	    action =REQUEST['URL2']+'/manage_main',
+	    target ='manage_main')
+
+    def external_name(self): return "%s.%s" % (self._module, self._function)
 
     def getFunction(self):
 
@@ -64,9 +92,10 @@ class ExternalMethod:
     
 class FuncCode:
 
-    def __init__(self, f):
-	self.co_varnames=f.func_code.co_varnames
-	self.co_argcount=f.func_code.co_argcount
+    def __init__(self, f=None):
+	if f is not None:
+	    self.co_varnames=f.func_code.co_varnames
+	    self.co_argcount=f.func_code.co_argcount
 
     def __cmp__(self,other):
 	return cmp((self.co_argcount, self.co_varnames),
