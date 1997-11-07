bf3726d5890a600704e90d25c2164f719902f585
Jim Fulton <jim@zope.com>
1997-11-07 19:31:42 +0000

    *** empty log message ***

---
+++ lib/python/OFS/__init__.py
@@ -0,0 +1,43 @@
+############################################################################## 
+#
+#     Copyright 
+#
+#       Copyright 1997 Digital Creations, L.C., 910 Princess Anne
+#       Street, Suite 300, Fredericksburg, Virginia 22401 U.S.A. All
+#       rights reserved.
+#
+############################################################################## 
+__doc__='''OFS
+$Id: __init__.py,v 1.1 1997/11/07 19:31:42 jim Exp $'''
+__version__='$Revision: 1.1 $'[11:-2]
+
+import Session
+
+__.meta_types=(
+    {'name':'Session', 'action':'manage_addSessionForm'},
+    {'name':'Image', 'action':'manage_addImageForm'},
+    {'name':'Folder', 'action':'manage_addFolderForm'},
+    {'name':'Document', 'action':'manage_addDocumentForm'},
+    )
+__.methods={
+    'manage_addSessionForm': Session.addForm,
+    'manage_addSession': Session.add,
+    }
+
+
+
+
+
+############################################################################## 
+#
+# $Log: __init__.py,v $
+# Revision 1.1  1997/11/07 19:31:42  jim
+# *** empty log message ***
+#
+# Revision 1.1  1997/10/09 17:34:53  jim
+# first cut, no testing
+#
+# Revision 1.1  1997/07/25 18:14:28  jim
+# initial
+#
+#
