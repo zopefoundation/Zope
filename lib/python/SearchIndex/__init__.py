bcdd742155426754eae4328a38a529481c454a6b
Jim Fulton <jim@zope.com>
1997-11-21 18:43:49 +0000

    hack, hack, hack sys.modules to make old pickles work. :-(

--- lib/python/SearchIndex/__init__.py
+++ lib/python/SearchIndex/__init__.py
@@ -11,33 +11,25 @@
 __doc__='''Collected utilities to support database indexing.
 
 
-$Id: __init__.py,v 1.1 1997/09/15 16:07:23 jim Exp $'''
-__version__='$Revision: 1.1 $'[11:-2]
+$Id: __init__.py,v 1.2 1997/11/21 18:43:49 jim Exp $'''
+__version__='$Revision: 1.2 $'[11:-2]
 
 import sys, string, os
 
 __path__=__.__path__
 
-for o in 's', 'r':
-    try: 
-	s=os.popen('uname -s').read()[:-1]
-	r=os.popen('uname -r').read()[:-1]
-	if r and s:
-	    p=(string.lower(s)+r)
-	    if p: __path__.append("%s/bin-%s/%s" % (
-		__path__[0], string.split(sys.version)[0], p))
-    except: pass
-
 __path__.append("%s/bin-%s/%s" % (
     __path__[0], string.split(sys.version)[0], sys.platform))
     
-    
-    
-
+for m in 'BTree', 'IIBTree', 'IOBTree', 'OIBTree', 'intSet':
+    sys.modules['SearchIndex.%s' % m]=__import__(m)
 
 ############################################################################## 
 #
 # $Log: __init__.py,v $
+# Revision 1.2  1997/11/21 18:43:49  jim
+# hack, hack, hack sys.modules to make old pickles work. :-(
+#
 # Revision 1.1  1997/09/15 16:07:23  jim
 # *** empty log message ***
 #
