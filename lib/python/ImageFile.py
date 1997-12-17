1d06b6826018aa1252df5c9b032846e6473482fc
Jim Fulton <jim@zope.com>
1997-12-17 16:36:57 +0000

    *** empty log message ***

---
+++ lib/python/ImageFile.py
@@ -0,0 +1,33 @@
+"""Image object that is stored in a file"""
+
+__version__='$Revision: 1.1 $'[11:-2]
+
+from string import rfind
+from Globals import package_home
+
+class ImageFile:
+    """Image object stored in an external file"""
+
+    def __init__(self,path,_prefix=None):
+	if _prefix is None: _prefix=SOFTWARE_HOME+'/lib/python'
+	elif type(_prefix) is not type(''): _prefix=package_home(_prefix)
+	path='%s/%s' % (_prefix, path)
+
+	self.path=path
+	self.content_type='image/%s' % path[rfind(path,'.')+1:]
+	self.__name__=path[rfind(path,'/')+1:]
+
+    def index_html(self, RESPONSE):
+	"""Default document"""
+	RESPONSE['content-type']=self.content_type
+	f=open(self.path)
+	data=f.read()
+	f.close()
+        return data
+
+    def __len__(self):
+	# This is bogus and needed because of the way Python tests truth.
+	return 1 
+
+    def __str__(self):
+	return '<IMG SRC="%s" ALT="%s">' % (self.__name__, self.title_or_id()) 
