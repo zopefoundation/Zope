c353413e781bee00f011e00b3e79e08e0a4a05b5
Jim Fulton <jim@zope.com>
1998-01-08 18:00:22 +0000

    *** empty log message ***

--- lib/python/App/Management.py
+++ lib/python/App/Management.py
@@ -1,10 +1,11 @@
+
 """Standard management interface support
 
-$Id: Management.py,v 1.10 1998/01/02 17:39:05 jim Exp $"""
+$Id: Management.py,v 1.11 1998/01/08 18:00:22 jim Exp $"""
 
-__version__='$Revision: 1.10 $'[11:-2]
+__version__='$Revision: 1.11 $'[11:-2]
 
-import sys,Globals
+import sys, Globals
 from Dialogs import MessageDialog
 from Globals import HTMLFile
 
@@ -13,6 +14,8 @@ class Tabs:
     manage_tabs     =HTMLFile('manage_tabs', globals())
     manage_options  =()
 
+Globals.default__class_init__(Tabs)
+
 class Navigation:
     """Basic (very) navigation UI support"""
 
@@ -20,3 +23,4 @@ class Navigation:
     manage_menu     =HTMLFile('menu', globals())
     manage_copyright=HTMLFile('copyright', globals())
 
+Globals.default__class_init__(Navigation)
