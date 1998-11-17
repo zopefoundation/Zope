a2cb98aeda7326484fe8799b18252378350b3395
Jim Fulton <jim@zope.com>
1998-11-17 20:23:42 +0000

    new copyright.

--- lib/Components/ExtensionClass/Sync.c
+++ lib/Components/ExtensionClass/Sync.c
@@ -1,55 +1,53 @@
-/***********************************************************
-     Copyright 
+/*
 
-       Copyright 1997 Digital Creations, L.L.C., 910 Princess Anne
-       Street, Suite 300, Fredericksburg, Virginia 22401 U.S.A. All
-       rights reserved.  Copyright in this software is owned by DCLC,
-       unless otherwise indicated.  Note that any product, process or
-       technology described in this software may be the subject of
-       other Intellectual Property rights reserved by Digital
-       Creations, L.C. and are not licensed hereunder.
+  Copyright (c) 1996-1998, Digital Creations, Fredericksburg, VA, USA.  
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+  
+    o Redistributions of source code must retain the above copyright
+      notice, this list of conditions, and the disclaimer that follows.
+  
+    o Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions, and the following disclaimer in
+      the documentation and/or other materials provided with the
+      distribution.
+  
+    o Neither the name of Digital Creations nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+  
+  
+  THIS SOFTWARE IS PROVIDED BY DIGITAL CREATIONS AND CONTRIBUTORS *AS
+  IS* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+  PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL DIGITAL
+  CREATIONS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+  DAMAGE.
 
-     Trademarks 
+  $Id: Sync.c,v 1.2 1998/11/17 20:22:34 jim Exp $
 
-       Digital Creations & DCLC, are trademarks of Digital Creations, L.C..
-       All other trademarks are owned by their respective companies. 
+  If you have questions regarding this software,
+  contact:
+ 
+    Digital Creations L.C.  
+    info@digicool.com
+ 
+    (540) 371-6909
 
-     No Warranty 
-
-       The software is provided "as is" without warranty of any kind,
-       either express or implied, including, but not limited to, the
-       implied warranties of merchantability, fitness for a particular
-       purpose, or non-infringement. This software could include
-       technical inaccuracies or typographical errors. Changes are
-       periodically made to the software; these changes will be
-       incorporated in new editions of the software. DCLC may make
-       improvements and/or changes in this software at any time
-       without notice.
-
-     Limitation Of Liability 
-
-       In no event will DCLC be liable for direct, indirect, special,
-       incidental, economic, cover, or consequential damages arising
-       out of the use of or inability to use this software even if
-       advised of the possibility of such damages. Some states do not
-       allow the exclusion or limitation of implied warranties or
-       limitation of liability for incidental or consequential
-       damages, so the above limitation or exclusion may not apply to
-       you.
-
-    If you have questions regarding this software,
-    contact:
-   
-      Digital Creations L.L.C.  
-      info@digicool.com
-      (540) 371-6909
-
-******************************************************************/
-
+*/
 
 static char Sync_module_documentation[] = 
 ""
-"\n$Id: Sync.c,v 1.1 1997/09/18 20:36:37 jim Exp $"
+"\n$Id: Sync.c,v 1.2 1998/11/17 20:22:34 jim Exp $"
 ;
 
 #include "ExtensionClass.h"
@@ -107,29 +105,25 @@ static struct PyMethodDef Synchronized_methods[] = {
   {"__call_method__", (PyCFunction)Synchronized___call_method__, METH_VARARGS,
    "Call a method by first getting a thread lock"
   },
-  {NULL,		NULL}		/* sentinel */
+  {NULL,		NULL}
 };
 
-/* List of methods defined in the module */
-
-static struct PyMethodDef Module_Level__methods[] = {
-  
-  {NULL, (PyCFunction)NULL, 0, NULL}		/* sentinel */
+static struct PyMethodDef Module_Level__methods[] = {  
+  {NULL, (PyCFunction)NULL, 0, NULL}
 };
-
-/* Initialization function for the module (*must* be called initSync) */
 
 void
 initSync()
 {
   PyObject *m, *d;
-  char *rev="$Revision: 1.1 $";
-  PURE_MIXIN_CLASS(Synchronized,
-		   "Mix-in class that provides synchonization of method calls\n"
-		   "\n"
-		   "Only one thread is allowed to call a synchronized \n"
-		   "object's methods.\n",
-		   Synchronized_methods);
+  char *rev="$Revision: 1.2 $";
+  PURE_MIXIN_CLASS(
+       Synchronized,
+       "Mix-in class that provides synchonization of method calls\n"
+       "\n"
+       "Only one thread is allowed to call a synchronized \n"
+       "object's methods.\n",
+       Synchronized_methods);
 
   UNLESS((lockstr=PyString_FromString("_sync__lock")) &&
 	 (aqstr=PyString_FromString("acquire")) &&
@@ -140,33 +134,14 @@ initSync()
   ASSIGN(newlock, PyObject_GetAttrString(newlock, "allocate_lock"));
   UNLESS(newlock) return;
 
-  /* Create the module and add the functions */
   m = Py_InitModule4("Sync", Module_Level__methods,
 		     Sync_module_documentation,
 		     (PyObject*)NULL,PYTHON_API_VERSION);
 
-  /* Add some symbolic constants to the module */
   d = PyModule_GetDict(m);
   PyExtensionClass_Export(d,"Synchronized",SynchronizedType);
   PyDict_SetItemString(d,"__version__",
 		       PyString_FromStringAndSize(rev+11,strlen(rev+11)-2));
 
-  /* Check for errors */
   CHECK_FOR_ERRORS("can't initialize module MethodObject");
 }
-
-/*****************************************************************************
-Revision Log:
-
-  $Log: Sync.c,v $
-  Revision 1.1  1997/09/18 20:36:37  jim
-  *** empty log message ***
-
-  $Revision 1.2  1997/06/21 15:22:03  jim
-  $Removed "object" suffix from generates C type names
-  $
-  $Revision 1.1  1997/02/24 23:25:42  jim
-  $initial
-  $
-
-*****************************************************************************/
