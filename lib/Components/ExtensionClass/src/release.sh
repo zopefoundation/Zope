e29a60d6e5bd32540ac1bfd040710b5e13349ac7
Jim Fulton <jim@zope.com>
1998-11-18 18:23:51 +0000

    *** empty log message ***

--- lib/Components/ExtensionClass/src/release.sh
+++ lib/Components/ExtensionClass/src/release.sh
@@ -9,6 +9,7 @@ StructuredText -t < Acquisition.stx > Acquisition.html
 StructuredText -t < MultiMapping.stx > MultiMapping.html
 StructuredText -t < COPYRIGHT.txt > COPYRIGHT.html
 StructuredText -t < release.notes > release.html
+StructuredText -t < index.stx > index.html
 
 rm -rf "$M-$R"
 mkdir  "$M-$R"
