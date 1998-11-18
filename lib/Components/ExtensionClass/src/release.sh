589034bca454635ac22ad1877a7d7b9ab17c3eff
Jim Fulton <jim@zope.com>
1998-11-18 14:17:51 +0000

    *** empty log message ***

--- lib/Components/ExtensionClass/src/release.sh
+++ lib/Components/ExtensionClass/src/release.sh
@@ -4,6 +4,11 @@ R=$1
 M=ExtensionClass
 
 StructuredText < $M.stx > $M.html
+StructuredText < Installation > Installation.html
+StructuredText < Acquisition.stx > Acquisition.html
+StructuredText < MultiMapping.stx > MultiMapping.html
+StructuredText < release.notes > release.html
+
 rm -rf "$M-$R"
 mkdir  "$M-$R"
 for f in `cat release.fl`; do
