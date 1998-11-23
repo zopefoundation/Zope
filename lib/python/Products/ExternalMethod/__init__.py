1e57152377b8edcc00482236639efe267a201498
Jim Fulton <jim@zope.com>
1998-11-23 16:41:16 +0000

    test of cvs

--- lib/python/Products/ExternalMethod/__init__.py
+++ lib/python/Products/ExternalMethod/__init__.py
@@ -8,8 +8,8 @@
 #
 ############################################################################## 
 __doc__='''External Method Product Initialization
-$Id: __init__.py,v 1.7 1998/05/20 21:18:49 jim Exp $'''
-__version__='$Revision: 1.7 $'[11:-2]
+$Id: __init__.py,v 1.8 1998/11/23 16:41:16 jim Exp $'''
+__version__='$Revision: 1.8 $'[11:-2]
 
 import ExternalMethod
 from ImageFile import ImageFile
@@ -36,32 +36,3 @@ __ac_permissions__=(
      ('manage_addExternalMethodForm', 'manage_addExternalMethod')),
     ('Change External Methods', ()),
     )
-
-
-############################################################################## 
-#
-# $Log: __init__.py,v $
-# Revision 1.7  1998/05/20 21:18:49  jim
-# Updated permissions.
-#
-# Revision 1.6  1998/02/23 18:28:36  jim
-# updated permissions
-#
-# Revision 1.5  1998/01/29 15:54:58  brian
-# Added eval support
-#
-# Revision 1.4  1997/12/23 22:15:34  jim
-# up to date with product and security layout
-#
-# Revision 1.3  1997/12/22 15:18:13  jeffrey
-# typo fix
-#
-# Revision 1.2  1997/12/19 22:18:16  jeffrey
-# fixes for icons
-#
-# Revision 1.1  1997/10/09 17:34:53  jim
-# first cut, no testing
-#
-# Revision 1.1  1997/07/25 18:14:28  jim
-# initial
-#
