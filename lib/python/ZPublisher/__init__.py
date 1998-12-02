e9dc2827625ba6203b3d96ff446ce372f95e4e5c
Jim Fulton <jim@zope.com>
1998-12-02 19:54:37 +0000

    Added test and Main

--- lib/python/ZPublisher/__init__.py
+++ lib/python/ZPublisher/__init__.py
@@ -1,1 +1,13 @@
 from Publish import publish_module
+
+def test(*args, **kw):
+    global test
+    import Test
+    test=Test.publish
+    return apply(test, args, kw)
+
+def Main(*args, **kw):
+    global test
+    import Test
+    test=Test.publish
+    return apply(test, ('Main',)+args, kw)
