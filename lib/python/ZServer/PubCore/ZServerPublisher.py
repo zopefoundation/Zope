7f20fdae38054dc6f0ed16149cb236a3052cc086
Amos Latteier <amos@latteier.com>
1999-01-09 03:11:01 +0000

    added module name to the request

--- lib/python/ZServer/PubCore/ZServerPublisher.py
+++ lib/python/ZServer/PubCore/ZServerPublisher.py
@@ -3,10 +3,10 @@ from ZPublisher import publish_module
 from cStringIO import StringIO
 
 class ZServerPublisher:
-    def __init__(self, name, accept):
+    def __init__(self, accept):
        while 1:
-           input, output, environ = accept()
            try:
+               name,input,output,environ=accept()
                publish_module(
                    name, 
                    stdin=input,
