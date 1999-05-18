3e24d4cf8b0ebe2b9ba72414836abbaa0ffb3929
Jim Fulton <jim@zope.com>
1999-05-18 15:47:40 +0000

    Added a version manager icon.

--- lib/python/OFS/misc_.py
+++ lib/python/OFS/misc_.py
@@ -109,6 +109,7 @@ class p_:
     ControlPanel_icon=ImageFile('OFS/www/ControlPanel_icon.gif')
     ApplicationManagement_icon=ImageFile('App/www/cpSystem.gif')
     DatabaseManagement_icon=ImageFile('App/www/dbManage.gif')
+    VersionManagement_icon=ImageFile('App/www/vManage.gif')
     InstalledProduct_icon=ImageFile('App/www/installedProduct.gif')
     BrokenProduct_icon=ImageFile('App/www/brokenProduct.gif')
     Product_icon=ImageFile('App/www/product.gif')
