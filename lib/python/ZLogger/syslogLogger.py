a8eb45f4834413a482710b19e304702b437d054c
Michel Pelletier <michel@svn.zope.org>
1999-07-16 17:16:59 +0000

    Fixed it so that syslog is not on by default.
    It broke windows.

--- lib/python/ZLogger/syslogLogger.py
+++ lib/python/ZLogger/syslogLogger.py
@@ -8,8 +8,13 @@ class syslogLogger:
         if os.environ.has_key('ZSYSLOG_SERVER'):
             (addr, port) = string.split(os.environ['ZSYSLOG_SERVER'], ':')
             self.client = syslog_client((addr, int(port)))
-        else:
+            self.on = 1
+        elif os.environ.has_key('ZSYSLOG'):
             self.client = syslog_client()
+            self.on = 1
+        else:
+            self.on = 0
 
     def __call__(self, sub, sev, sum, det, err):
-        self.client.log(sum)
+        if on:
+            self.client.log(sum)
