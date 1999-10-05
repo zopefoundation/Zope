5845a0b9d2e6197d0caa28126730a68e4369b4fa
Amos Latteier <amos@latteier.com>
1999-10-05 20:21:18 +0000

    Changed tabs to spaces and added a flush to log writing.

--- lib/python/ZServer/DebugLogger.py
+++ lib/python/ZServer/DebugLogger.py
@@ -86,48 +86,47 @@
 import time, thread
 
 class DebugLogger:
-	"""
-	Logs debugging information about how ZServer is handling requests
-	and responses. This log can be used to help locate troublesome requests.
-	
-	The format is:
-	
-		<code> <request id> <time> <data>
-	
-	where:
-	
-		'code' is B for begin, I for received input, A for received output,
-			E for sent output.
-		
-		'request id' is a unique request id.
-		
-		'time' is the time in GMT ISO format.
-		
-		'data' is the HTTP method and the PATH INFO for B, the size of the input
-			for I, the HTTP status code and the size of the output for A, or
-			nothing for E.
-			
-	Note: This facility will be probably be adapted to the zLOG framework.
-	"""
-	
-	def __init__(self, filename):
-		self.file=open(filename, 'a+b')
-		l=thread.allocate_lock()
-		self._acquire=l.acquire
-		self._release=l.release
-		
-	def log(self, code, request_id, data=''):
-		self._acquire()
-		try:
-			t=time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(time.time()))
-			self.file.write(
-				'%s %s %s %s\n' % (code, request_id, t, data)
-				)
-		finally:
-			self._release()
+    """
+    Logs debugging information about how ZServer is handling requests
+    and responses. This log can be used to help locate troublesome requests.
+    
+    The format is:
+    
+        <code> <request id> <time> <data>
+    
+    where:
+    
+        'code' is B for begin, I for received input, A for received output,
+            E for sent output.
+        
+        'request id' is a unique request id.
+        
+        'time' is the time in GMT ISO format.
+        
+        'data' is the HTTP method and the PATH INFO for B, the size of the input
+            for I, the HTTP status code and the size of the output for A, or
+            nothing for E.
+            
+    Note: This facility will be probably be adapted to the zLOG framework.
+    """
+    
+    def __init__(self, filename):
+        self.file=open(filename, 'a+b')
+        l=thread.allocate_lock()
+        self._acquire=l.acquire
+        self._release=l.release
+        
+    def log(self, code, request_id, data=''):
+        self._acquire()
+        try:
+            t=time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(time.time()))
+            self.file.write(
+                '%s %s %s %s\n' % (code, request_id, t, data)
+                )
+            self.file.flush()
+        finally:
+            self._release()
 
 
 def log(*args): pass
 
-		
-		
