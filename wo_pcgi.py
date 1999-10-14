3c2b5f7e6f015e584ab7db18472fd0c4c7a22067
Ken Manheimer <ken.manheimer@gmail.com>
1999-10-14 14:50:09 +0000

    Move the zpasswd activity to below the permissions setting ones, particularly so the password report doesn't get scrolled off the screen!
    I'm removing the default_perms line - the permissions setting stuff
    has gotten combined into the default_content module.

--- wo_pcgi.py
+++ wo_pcgi.py
@@ -103,9 +103,8 @@ def main(me):
     import compilezpy
     import build_extensions
     user=group=''
-    import zpasswd; zpasswd.write_access(home, user, group)
-    import default_content; default_content.main(home, user, group)
     import default_perms; default_perms.main(home, user, group)
+    import zpasswd; zpasswd.write_access(home, user, group)
     
     pcgi=os.path.join(home, 'Zope.cgi')
     import make_start; make_start.sh(home, user, group)
