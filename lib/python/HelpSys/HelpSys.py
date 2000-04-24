3022a300a16fff4e519f4065e3fb5440c8373634
Amos Latteier <amos@latteier.com>
2000-04-24 23:53:16 +0000

    Moved help system code to the HelpSys module. Removed the HelpSystem module. This was done to improve backword compatibility with Zope 2.1.6 and earlier.
    Zope's built recently from CVS may have problems with this change. If I hear a lot of complaints I may add backward compatibility, otherwise it should be easy enough fix by either erasing your database or replacing your HelpSys object.

--- lib/python/HelpSys/HelpSys.py
+++ lib/python/HelpSys/HelpSys.py
@@ -82,54 +82,165 @@
 # attributions are listed in the accompanying credits file.
 # 
 ##############################################################################
-"""Help system implementation"""
 
-__version__='$Revision: 1.7 $'[11:-2]
+import Acquisition
+from OFS.SimpleItem import Item
+from OFS.ObjectManager import ObjectManager
+from Globals import Persistent, HTMLFile, HTML
+from Products.ZCatalog.ZCatalog import ZCatalog
+from Products.ZCatalog.Lazy import LazyCat
+import Products
+import HelpTopic
 
-
-import sys, os, string, Globals, Acquisition
-from HelpUtil import HelpBase
-from ObjectRef import ObjectRef
-from App.ImageFile import ImageFile
-from Globals import HTMLFile
-
-
-
-
-
-class HelpSys(HelpBase):
-    """ """
-    __names__=None
-    __roles__=None
-
-    hs_index=HTMLFile('helpsys', globals())
-    hs_menu =HTMLFile('helpsys_menu', globals())
-    hs_main =HTMLFile('helpsys_main', globals())
-
-    hs_cicon='HelpSys/hs_cbook'
-    hs_eicon='HelpSys/hs_obook'
+class HelpSys(Acquisition.Implicit, ObjectManager, Item, Persistent):
+    """
+    Zope Help System
     
-    # Images used by the help system
-    hs_uarrow=ImageFile('images/hs_uarrow.gif', globals())
-    hs_darrow=ImageFile('images/hs_darrow.gif', globals())
-    hs_rarrow=ImageFile('images/hs_rarrow.gif', globals())
-    hs_larrow=ImageFile('images/hs_larrow.gif', globals())
-    hs_dnode=ImageFile('images/hs_dnode.gif', globals())
-    hs_obook=ImageFile('images/hs_obook.gif', globals())
-    hs_cbook=ImageFile('images/hs_cbook.gif', globals())
+    Provides browsing and searching of Zope Product Help.
+    """
+    meta_type='Help System'
+
+    manage_options=(
+        {'label' : 'Contents', 'action' : 'menu'},
+        {'label' : 'Search', 'action' : 'search'},
+    )
+
+    __ac_permissions__=(
+        ('View',
+         ('__call__', 'searchResults', 'HelpButton', '',
+          'index_html', 'menu', 'search', 'results', 'main', )),
+        ('Access contents information', ('helpValues',)),
+        )
+
+    def __init__(self, id):
+        self.id=id
+
+    def helpValues(self, spec=None):
+        "ProductHelp objects of all Products that have help"
+        hv=[]
+        for product in self.Control_Panel.Products.objectValues():
+            productHelp=product.getProductHelp()
+            # only list products that actually have help
+            if productHelp.helpValues():
+                hv.append(productHelp)
+        return hv
+
+    # Seaching does an aggregated search of all ProductHelp
+    # objects. Only Help Topics for which the user has permissions
+    # are returned.
+    def __call__(self, REQUEST=None, **kw):
+        "Searchable interface"
+        if REQUEST is not None:
+            perms=[]
+            user=REQUEST.AUTHENTICATED_USER
+            for p in self.ac_inherited_permissions():
+                if user.has_permission(p[0], self):
+                    perms.append(p[0])
+            REQUEST.set('permissions',perms)
+        results=[]
+        for ph in self.helpValues():
+            results.append(apply(getattr(ph, '__call__'), (REQUEST,) , kw))
+        return LazyCat(results)   
+        
+    searchResults=__call__
     
-    hs_id='HelpSys'
-    hs_title='Z Help Online'
+    index_html=HTMLFile('frame', globals())
+    menu=HTMLFile('menu', globals())
+    search=HTMLFile('search', globals())
+    results=HTMLFile('results', globals())
+    main=HTML("""<html></html>""")
 
-    ObjectRef=ObjectRef()
+    button=HTMLFile('button', globals())
 
-    hs_objectvalues__roles__=None
-    def hs_objectvalues(self):
-        return [self.ObjectRef]
+    def HelpButton(self, topic, product):
+        """
+        Insert a help button linked to a help topic.
+        """
+        return self.button(self, self.REQUEST, product=product, topic=topic)
 
-    def __len__(self):
-        return 1
+    helpURL=HTMLFile('helpURL',globals())
 
 
 
+class ProductHelp(Acquisition.Implicit, ObjectManager, Item, Persistent):
+    """
+    Manages a collection of Help Topics for a given Product.
+    
+    Provides searching services to HelpSystem.
+    """
 
+    meta_type='Product Help'
+    icon='p_/ProductHelp_icon'
+
+    lastRegistered=None
+    
+    meta_types=({'name':'Help Topic',
+                 'action':'addTopicForm',
+                 'permission':'Add Documents, Images, and Files'},
+                )
+
+    manage_options=(
+        {'label':'Contents', 'action':'manage_main'},
+        {'label':'Import/Export', 'action':'manage_importExportForm'},
+        {'label':'Undo', 'action':'manage_UndoForm'},
+        )
+
+    __ac_permissions__=(
+        ('Add Documents, Files, and Images', ('addTopicForm', 'addTopic')),
+        )
+    
+    def __init__(self, id='Help', title=''):
+        self.id=id
+        self.title=title
+        self.catalog=ZCatalog('catalog')
+        c=self.catalog._catalog
+        # clear catalog
+        for index in c.indexes.keys():
+            c.delIndex(index)
+        for col in c.schema.keys():
+            c.delColumn(col)
+        c.addIndex('SearchableText', 'TextIndex')
+        c.addIndex('categories', 'KeywordIndex')
+        c.addIndex('permissions', 'KeywordIndex')
+        c.addColumn('categories')
+        c.addColumn('permissions')
+        c.addColumn('title_or_id')
+        c.addColumn('url')
+        c.addColumn('id')
+
+    addTopicForm=HTMLFile('addTopic', globals())
+
+    def addTopic(self, id, title, REQUEST=None):
+        "Add a Help Topic"
+        topic=HelpTopic.DTMLDocumentTopic(
+            HelpTopic.default_topic_content, __name__=id)
+        topic.title=title
+        self._setObject(id, topic)
+        if REQUEST is not None:
+            return self.manage_main(self, REQUEST,
+                                    manage_tabs_message='Help Topic added.')
+
+    def helpValues(self, REQUEST=None):
+        """
+        Lists contained Help Topics.
+        Help Topics for which the user is not authorized
+        are not listed.
+        """
+        topics=self.objectValues('Help Topic')
+        if REQUEST is None:
+            return topics
+        return filter(
+            lambda ht, u=REQUEST.AUTHENTICATED_USER: ht.authorized(u), topics)
+
+    def all_meta_types(self):
+        f=lambda x: x['name'] in ('Image', 'File')
+        return filter(f, Products.meta_types) + self.meta_types
+
+    def __call__(self, *args, **kw):
+        """
+        Searchable interface
+        """
+        return apply(self.catalog.__call__, args, kw)
+
+    standard_html_header=HTMLFile('topic_header', globals())
+    standard_html_footer=HTMLFile('topic_footer', globals())
