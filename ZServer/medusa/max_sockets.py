1dc79c3f0ad4b70679ef0b3fc7e9f31e45765fe3
Shane Hathaway <shane@hathawaymix.org>
2000-05-19 17:12:41 +0000

    Collector 1260 - major speedup in start time.

--- ZServer/medusa/max_sockets.py
+++ ZServer/medusa/max_sockets.py
@@ -50,18 +50,17 @@ def max_select_sockets():
 	sl = []
 	while 1:
 		try:
-			s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-			s.bind (('',0))
-			s.listen(5)
-			sl.append (s)
+			num = len(sl)
+			for i in range(1 + len(sl) * 0.05):
+				# Increase exponentially.
+				s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+				s.bind (('',0))
+				s.listen(5)
+				sl.append (s)
 			select.select(sl,[],[],0)
 		except:
 			break
-	num = len(sl) - 1
 	for s in sl:
 		s.close()
 	del sl
 	return num
-	
-	
-
