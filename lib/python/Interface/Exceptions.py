879cd1dc4e83d69f9d64238cdccb59dbae4bc47c
Michel Pelletier <michel@svn.zope.org>
2000-11-30 17:14:45 +0000

    changed exceptions

--- lib/python/Interface/Exceptions.py
+++ lib/python/Interface/Exceptions.py
@@ -27,14 +27,13 @@ class BrokenMethodImplementation(Exception):
     """An method is not completely implemented.
     """
 
-    def __init__(self, method, name):
+    def __init__(self, method):
         self.method=method
-        self.name=name
 
     def __str__(self):
         return """An object has failed to implement the method %(method)s
 
-        The %(name)s signature attribute is incorrect.
+        The signature is incorrect.
         """ % self.__dict__
 
 class InvalidInterface(Exception): pass
