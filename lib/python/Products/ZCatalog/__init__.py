de4b0a64c8ef0a79f7bcb94b00d9978c983d8ed1
Christopher Petrilli <petrilli@amber.org>
2001-01-15 16:39:31 +0000

    Minor update to make sure the name showing up in the Add List is 'Z Catalog' which has the same naming scheme as all other Z* objects. This won't affect the searing based on meta_type, just the thing that's listed in the drop down list.

--- lib/python/Products/ZCatalog/__init__.py
+++ lib/python/Products/ZCatalog/__init__.py
@@ -100,6 +100,7 @@ def initialize(context):
         constructors=(ZCatalog.manage_addZCatalogForm,
                       ZCatalog.manage_addZCatalog),
         icon='www/ZCatalog.gif',
+        meta_type='Z Catalog',
         )
 
     context.registerClass(
