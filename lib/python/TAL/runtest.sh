34afc37d495a35409359f38cafbb7d8dc492fd9c
Guido van Rossum <guido@python.org>
2001-01-29 15:26:09 +0000

    - Change shell used to /bin/sh. - Recognize command line options.

--- lib/python/TAL/runtest.sh
+++ lib/python/TAL/runtest.sh
@@ -1,14 +1,24 @@
-#! /bin/bash
+#! /bin/sh
 
 # Run the regression test suite
 
 : ${TMPDIR=/tmp}
 
+flags=""
+
+while :
+do
+    case $1 in
+    -*) flags="$flags $1"; shift;;
+    *)  break;;
+    esac
+done
+
 for test in ${*-test/test*.xml}
 do
     out=` echo $test | sed 's,/[^/0-9]*\([0-9]*\).xml,/out\1.xml,' `
     tmp=$TMPDIR/taltest$$`basename $test`
-    ./driver.py $test >$tmp
+    ./driver.py $flags $test >$tmp
     if cmp -s $tmp $out
     then
         echo $test OK
