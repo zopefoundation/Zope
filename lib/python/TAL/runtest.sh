e3c295293195e7ad39bd9a62d79231066143fcda
Guido van Rossum <guido@python.org>
2001-01-29 20:50:22 +0000

    Only run diff if the test program terminated normally, with exit status 0.

--- lib/python/TAL/runtest.sh
+++ lib/python/TAL/runtest.sh
@@ -19,12 +19,16 @@ do
     out=` echo $test | sed 's,/[^/0-9]*\([0-9]*\).xml,/out\1.xml,' `
     tmp=$TMPDIR/taltest$$`basename $test`
     ./driver.py $flags $test >$tmp
-    if cmp -s $tmp $out
-    then
-        echo $test OK
-    else
-        echo "$test failed -- diff (expected vs. actual) follows"
-        diff $out $tmp
-    fi
+    case $? in
+    0)  if cmp -s $tmp $out
+	then
+	    echo $test OK
+	else
+	    echo "$test failed -- diff (expected vs. actual) follows"
+	    diff $out $tmp
+	fi
+	;;
+    *) echo $test exit status $?;;
+    esac
     rm $tmp
 done
