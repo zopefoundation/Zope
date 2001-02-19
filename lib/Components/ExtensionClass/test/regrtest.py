8270dce131ffba168063ff7d557d92b8a71f3917
Jeremy Hylton <jeremy@svn.zope.org>
2001-02-19 19:20:26 +0000

    Don't call test.regrtest.main() with explicit list of test cases, so that main() can handle argument processing entirely.
    Add test_AqAlg to list of tests.

--- lib/Components/ExtensionClass/test/regrtest.py
+++ lib/Components/ExtensionClass/test/regrtest.py
@@ -4,10 +4,14 @@ import os
 import sys
 import test.regrtest
 
-ec_tests = ["test_MultiMapping", "test_Sync", "test_ThreadLock",
-            "test_acquisition", "test_add", "test_binding",
-            "test_explicit_acquisition", "test_func_attr",
-            "test_method_hook"]
+ec_tests = ["test_AqAlg", "test_MultiMapping", "test_Sync",
+            "test_ThreadLock", "test_acquisition", "test_add",
+            "test_binding", "test_explicit_acquisition",
+            "test_func_attr", "test_method_hook"]
 
-ec_testdir = os.path.split(sys.argv[0])[0]
-test.regrtest.main(tests=ec_tests, testdir=ec_testdir)
+ec_testdir = os.path.split(sys.argv[0])[0] or '.'
+
+test.regrtest.STDTESTS = ec_tests
+test.regrtest.NOTTESTS = []
+
+test.regrtest.main(testdir=ec_testdir)
