0cc6d3516d6dbce129956600fb4971daac5faa9e
Barry Warsaw <barry@python.org>
2001-03-16 22:19:41 +0000

    PyUnit test suite framework for BSDDB storage tests.

---
+++ lib/python/BDBStorage/tests/testall.py
@@ -0,0 +1,19 @@
+# Framework for running Unit tests
+
+import sys
+import unittest
+
+MODULES = ('commitlog', 'create', 'virgin')
+
+
+
+def suite():
+    alltests = unittest.TestSuite()
+    for mod in [__import__('test_'+mod) for mod in MODULES]:
+        alltests.addTest(mod.suite())
+    return alltests
+
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
