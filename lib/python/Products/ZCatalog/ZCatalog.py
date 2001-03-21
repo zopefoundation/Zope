a78dc107e24256c6dbce92dee42789c91be0f4ae
Chris McDonough <chrism@plope.com>
2001-03-21 21:47:04 +0000

    Merged Jim's change to __init__ to change self to self.__of__(container) into trunk.

--- lib/python/Products/ZCatalog/ZCatalog.py
+++ lib/python/Products/ZCatalog/ZCatalog.py
@@ -224,6 +224,8 @@ class ZCatalog(Folder, Persistent, Implicit):
     _v_transaction = None
     
     def __init__(self, id, title='', vocab_id=None, container=None):
+        self=self.__of__(container)
+        
         self.id=id
         self.title=title
         
