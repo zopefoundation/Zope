50aa72fee3409244391d035a577160725d551a5e
Jim Fulton <jim@zope.com>
2001-04-07 17:28:17 +0000

    ecided we didn't need to use Python's version after all. (There's really not a good reason for asynchat to be in Python's standard lib.)

--- lib/python/ZServer/async1x/__init__.py
+++ lib/python/ZServer/async1x/__init__.py
@@ -2,10 +2,8 @@
 # We want to use updated asynchat and asyncore if using Python1.5
 import sys
 if sys.version[:1] < '2':
-    import asyncore, asynchat
+    import asyncore
     sys.modules['asyncore']=asyncore
-    sys.modules['asynchat']=asynchat
     del asyncore
-    del asynchat
 
 del sys
