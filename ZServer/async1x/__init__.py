a5e4236edf2607e2dc41625a256c697726eba369
Andreas Jung <yet@gmx.de>
2001-04-25 19:31:05 +0000

    removed (no more needed for Python 2.1)

--- ZServer/async1x/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# We want to use updated asynchat and asyncore if using Python1.5
-import sys
-if sys.version[:1] < '2':
-    import asyncore
-    sys.modules['asyncore']=asyncore
-    del asyncore
-
-del sys
