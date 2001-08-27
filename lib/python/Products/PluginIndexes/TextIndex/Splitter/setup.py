dbcec07f5876b1760be4d2601d4d14e6c0c2b7f1
Andreas Jung <yet@gmx.de>
2001-08-27 13:14:25 +0000

    *** empty log message ***

--- lib/python/Products/PluginIndexes/TextIndex/Splitter/setup.py
+++ lib/python/Products/PluginIndexes/TextIndex/Splitter/setup.py
@@ -13,10 +13,10 @@ setup (name = "Splitter",
        version = "1.0",
        description = "Splitters for Zope 2.4",
        author = "Andreas Jung",
-       author_email = "andreas@digicool.com",
+       author_email = "andreas@zope.com",
        url = "http://www.zope.org/...",        
-       ext_modules=[Extension("Splitter",['src/Splitter.c']), \
-                    Extension("ISO_8859_1_Splitter",['src/ISO_8859_1_Splitter.c'])   \
+       ext_modules=[Extension("ZopeSplitter",['ZopeSplitter/src/ZopeSplitter.c']), \
+                    Extension("ISO_8859_1_Splitter",['ISO_8859_1_Splitter/src/ISO_8859_1_Splitter.c'])   \
                    ]
  
 
