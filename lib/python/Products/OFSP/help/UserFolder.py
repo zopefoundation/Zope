0ae0fa28bf854450e68f238e212a10ad04c88b44
Amos Latteier <amos@latteier.com>
2001-09-17 22:04:31 +0000

    changed UserFolderAPI.py to UserFolder.py and fixed some formatting nits

--- lib/python/Products/OFSP/help/UserFolder.py
+++ lib/python/Products/OFSP/help/UserFolder.py
@@ -1,38 +1,35 @@
-class UserFolderAPI:
+class UserFolder:
     """
     User Folder objects are containers for user objects. Programmers can
     work with collections of user objects using the API shared by User
     Folder implementations.
     """
 
-    def getUser(self, name):
+    def getUser(name):
         """
         Returns the user object specified by name.  If there is no
         user named 'name' in the user folder, return None.
 
         Permission -- Manage users
-
         """
 
-    def getUsers(self):
+    def getUsers():
         """
         Returns a sequence of all user objects which reside in the user
         folder.
 
         Permission -- Manage users
-        
         """
 
-    def getUserNames(self):
+    def getUserNames():
         """
         Returns a sequence of names of the users which reside in the user
         folder.
 
         Permission -- Manage users
-
         """
 
-    def manage_addUser(self, name, password, roles, domains):
+    def manage_addUser(name, password, roles, domains):
         """
         API method for creating a new user object. Note that not all
         user folder implementations support dynamic creation of user
@@ -40,11 +37,9 @@ class UserFolderAPI:
         of user objects will raise an error for this method.
 
         Permission -- Manage users
-
         """
 
-
-    def manage_editUser(self, name, password, roles, domains):
+    def manage_editUser(name, password, roles, domains):
         """
         API method for changing user object attributes. Note that not
         all user folder implementations support changing of user object
@@ -52,11 +47,9 @@ class UserFolderAPI:
         object attributes will raise an error for this method.
 
         Permission -- Manage users
-
         """
 
-
-    def manage_delUsers(self, names):
+    def manage_delUsers(names):
         """
         API method for deleting one or more user objects. Note that not
         all user folder implementations support deletion of user objects.
@@ -64,5 +57,4 @@ class UserFolderAPI:
         will raise an error for this method.
 
         Permission -- Manage users
-
         """
