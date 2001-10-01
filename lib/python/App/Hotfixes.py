57f2028231932b7147e5cf406fb31c83b0042ea4
Shane Hathaway <shane@hathawaymix.org>
2001-10-01 15:31:50 +0000

    Updated for clarity.

--- lib/python/App/Hotfixes.py
+++ lib/python/App/Hotfixes.py
@@ -3,7 +3,13 @@ from version_txt import getZopeVersion
 from zLOG import LOG, INFO, WARNING
 
 merged_hotfixes = {
+    'Hotfix_2001-09-28': 1
     }
+
+
+APPLY = 1
+ALREADY_MERGED = 0
+OUTDATED_ZOPE = None
 
 
 def isMerged(name):
@@ -11,28 +17,29 @@ def isMerged(name):
 
 
 def logHotfix(id, apply_hotfix):
-    if apply_hotfix > 0:
+    if apply_hotfix:
         LOG('Hotfixes', INFO, 'Applying %s' % id)
-    elif apply_hotfix < 0:
+    elif apply_hotfix is OUTDATED_ZOPE:
         LOG('Hotfixes', WARNING, 'Not applying %s.  It is not designed for '
             'this version of Zope.  Please uninstall the hotfix product.'
             % id)
-    else:
+    else:  # ALREADY_MERGED
         LOG('Hotfixes', WARNING, 'Not applying %s.  The fix has already been '
             'merged into Zope.  Please uninstall the hotfix product.'
             % id)
 
 
 def beforeApplyHotfix(id, req_major, req_minor, req_micro):
-    apply_hotfix = 0
     major, minor, micro = getZopeVersion()[:3]
     if major > 0 and (
         (major * 10000 + minor * 100 + micro) <
         (req_major * 10000 + req_minor * 100 + req_micro)):
         # The version of Zope is too old for this hotfix.
-        apply_hotfix = -1
-    elif not isMerged(id):
-        apply_hotfix = 1
+        apply_hotfix = OUTDATED_ZOPE
+    elif isMerged(id):
+        apply_hotfix = ALREADY_MERGED
+    else:
+        apply_hotfix = APPLY
     logHotfix(id, apply_hotfix)
     return apply_hotfix
 
