793a99954be5f05392941ad514f7c041ec4a5281
Andreas Jung <yet@gmx.de>
2001-10-16 12:46:11 +0000

    <a href="index_html">index_html</a> got transformed to <a href="index<u>html">index</u>html</a> (reported by Amos)

--- lib/python/StructuredText/STletters.py
+++ lib/python/StructuredText/STletters.py
@@ -12,6 +12,6 @@ letters     = string.letters
 literal_punc = punc_func("'")
 dbl_quoted_punc = punc_func("\"")
 strongem_punc = punc_func('*')
-under_punc = punc_func('_')
+under_punc = punc_func('_<>')
 phrase_delimiters = r'\s\.\,\?\/\!\&\(\)'
 
