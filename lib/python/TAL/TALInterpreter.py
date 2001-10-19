324922bbf0bdb1d43f4f8b6a2e14198ab9e776fa
Jeremy Hylton <jeremy@svn.zope.org>
2001-10-19 18:09:54 +0000

    Replace string/strip calls with string methods.
    This silences a DeprecationWarning when running the Zope tests.  It's
    kosher, because the trunk requires Python 2.1.

--- lib/python/TAL/TALInterpreter.py
+++ lib/python/TAL/TALInterpreter.py
@@ -90,11 +90,6 @@ import sys
 import getopt
 
 from cgi import escape
-from string import join, lower, rfind
-try:
-    from strop import lower, rfind
-except ImportError:
-    pass
 
 try:
     from cStringIO import StringIO
@@ -235,9 +230,9 @@ class TALInterpreter:
             self.col = 0
 
     def stream_write(self, s,
-                     len=len, rfind=rfind):
+                     len=len):
         self._stream_write(s)
-        i = rfind(s, '\n')
+        i = s.rfind('\n')
         if i < 0:
             self.col = self.col + len(s)
         else:
@@ -338,7 +333,7 @@ class TALInterpreter:
             # Clear 'entering' flag
             macs[-1][2] = 0
             # Convert or drop depth-one METAL attributes.
-            i = rfind(name, ":") + 1
+            i = name.rfind(":") + 1
             prefix, suffix = name[:i], name[i:]
             if suffix == "define-macro":
                 # Convert define-macro as we enter depth one.
@@ -362,7 +357,7 @@ class TALInterpreter:
         if action > 1:
             return self.attrAction(item)
         ok = 1
-        if self.html and lower(name) in BOOLEAN_HTML_ATTRS:
+        if self.html and name.lower() in BOOLEAN_HTML_ATTRS:
             evalue = self.engine.evaluateBoolean(item[3])
             if evalue is self.Default:
                 if action == 1: # Cancelled insert
@@ -491,7 +486,7 @@ class TALInterpreter:
             return
         s = escape(text)
         self._stream_write(s)
-        i = rfind(s, '\n')
+        i = s.rfind('\n')
         if i < 0:
             self.col = self.col + len(s)
         else:
