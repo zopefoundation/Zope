1387bfb5d20dfeacdfdc1f614de5b93576ea517d
Amos Latteier <amos@latteier.com>
2001-11-20 01:08:51 +0000

    Clean up transient object API docs. Still need permissions for transient objects.

--- lib/python/Products/Transience/help/TransienceInterfaces.py
+++ lib/python/Products/Transience/help/TransienceInterfaces.py
@@ -77,181 +77,48 @@
 """
 Transient Objects
 
-  TransientObjectContainers implement:
-
-    - ItemWithId
-
-    - StringKeyedHomogenousItemContainer
-
-    - TransientItemContainer
-
-  In particular, one uses the 'new__or__existing' method on
-  TransientObjectContainers to retrieve or create a TransientObject based
-  on a given string key.  
-
-  If add or delete notifications are registered with the container, they
-  will be called back when items in the container are added or deleted,
-  with the item and the container as arguments.  The callbacks may be
-  registered either as bound methods, functions, or named paths in Zope.
-
-  TransientObjects implement:
-
-    - ItemWithId
-
-    - Transient
-
-    - DictionaryLike
-
-    - TTWDictionary
-
-    - ImmutablyValuedMappingOfPickleableObjects
-
 """
 
-import Interface
+class TransientObjectContainer(Interface.Base):
+    """
+    TransientObjectContainers hold transient objects, most often,
+    session data.
 
-class Transient(Interface.Base):
-    def invalidate(self):
-        """
-        Invalidate (expire) the transient object.
-
-        Causes the transient object container's "before destruct" method
-        related to this object to be called as a side effect.
-        """
-
-    def getLastAccessed(self):
-        """
-        Return the time the transient object was last accessed in
-        integer seconds-since-the-epoch form.
-        """
-
-    def setLastAccessed(self):
-        """
-        Cause the last accessed time to be set to now.
-        """
-
-    def getCreated(self):
-        """
-        Return the time the transient object was created in integer
-        seconds-since-the-epoch form.
-        """
-
-class DictionaryLike(Interface.Base):
-    def keys(self):
-        """
-        Return sequence of key elements.
-        """
-
-    def values(self):
-        """
-        Return sequence of value elements.
-        """
-
-    def items(self):
-        """
-        Return sequence of (key, value) elements.
-        """
-
-    def get(self, k, default='marker'):
-        """
-        Return value associated with key k.  If k does not exist and default
-        is not marker, return default, else raise KeyError.
-        """
-
-    def has_key(self, k):
-        """
-        Return true if item referenced by key k exists.
-        """
-
-    def clear(self):
-        """
-        Remove all key/value pairs.
-        """
-
-    def update(self, d):
-        """
-        Merge dictionary d into ourselves.
-        """
-
-    # DictionaryLike does NOT support copy()
-
-class ItemWithId(Interface.Base):
+    You will rarely have to script a transient object
+    container. You'll almost always deal with a TransientObject
+    itself which you'll usaually get as 'REQUEST.SESSION'.
+    """
+    
     def getId(self):
         """
         Returns a meaningful unique id for the object.
-        """
 
-class TTWDictionary(DictionaryLike, ItemWithId):
-    def set(self, k, v):
-        """
-        Call _&nbsp;&nbsp;_setitem_&nbsp;&nbsp;_ with key k, value v.
-        """
-
-    def delete(self, k):
-        """
-        Call _&nbsp;&nbsp;_delitem_&nbsp;&nbsp;_ with key k.
-        """
-
-    def __guarded_setitem__(self, k, v):
-        """
-        Call _&nbsp;&nbsp;_setitem_&nbsp;&nbsp;_ with key k, value v.
-        """
-
-class ImmutablyValuedMappingOfPickleableObjects(Interface.Base):
-    def __setitem__(self, k, v):
-        """
-        Sets key k to value v, if k is both hashable and pickleable and
-        v is pickleable, else raise TypeError.
-        """
-
-    def __getitem__(self, k):
-        """
-        Returns the value associated with key k.
-
-        Note that no guarantee is made to persist changes made to mutable
-        objects obtained via _&nbsp;&nbsp;_getitem_&nbsp;&nbsp;_, even if
-        they support the ZODB Persistence interface.  In order to ensure
-        that changes to mutable values are persisted, you need to explicitly
-        put the value back in to the mapping via the
-        _&nbsp;&nbsp;_setitem_&nbsp;&nbsp;_.
-        """
-
-    def __delitem__(self, k):
-        """
-        Remove the key/value pair related to key k.
-        """
-
-class HomogeneousItemContainer(Interface.Base):
-    """
-    An object which:
-     1.  Contains zero or more subobjects, all of the same type.
-     2.  Is responsible for the creation of its subobjects.
-     3.  Allows for the access of a subobject by key.
-    """
-    def getSubobjectInterface(self):
-        """
-        Returns the interface object which must be supported by items added
-        to or created by this container.
+        Permission -- Always available
         """
 
     def get(self, k, default=None):
         """
         Return value associated with key k.  If value associated with k does
         not exist, return default.
+
+        Permission -- 'Access Transient Objects'
         """
 
     def has_key(self, k):
         """
         Return true if container has value associated with key k, else
         return false.
+
+        Permission -- 'Access Transient Objects'
         """
 
     def delete(self, k):
         """
         Delete value associated with key k, raise a KeyError if nonexistent.
+
+        Permission -- 'Access Transient Objects'
         """
 
-class StringKeyedHomogeneousItemContainer(HomogeneousItemContainer):
     def new(self, k):
         """
         Creates a new subobject of the type supported by this container
@@ -261,6 +128,8 @@ class StringKeyedHomogeneousItemContainer(HomogeneousItemContainer):
         KeyError is raised.
 
         "k" must be a string, else a TypeError is raised.
+
+        Permission -- 'Create Transient Objects'
         """
 
     def new_or_existing(self, k):
@@ -272,24 +141,31 @@ class StringKeyedHomogeneousItemContainer(HomogeneousItemContainer):
         container with key "k" and return it.
 
         "k" must be a string, else a TypeError is raised.
+
+        Permission -- 'Create Transient Objects'
         """
     
-class TransientItemContainer(Interface.Base):
     def setTimeoutMinutes(self, timeout_mins):
         """
         Set the number of minutes of inactivity allowable for subobjects
         before they expire.
+
+        Permission -- 'Manage Transient Object Container'
         """
 
     def getTimeoutMinutes(self):
         """
         Return the number of minutes allowed for subobject inactivity
         before expiration.
+
+        Permission -- 'View management screens'
         """
 
     def getAddNotificationTarget(self):
         """
         Returns the current 'after add' function, or None.
+
+        Permission -- 'View management screens'
         """
 
     def setAddNotificationTarget(self, f):
@@ -301,11 +177,15 @@ class TransientItemContainer(Interface.Base):
 
         'after add' functions need accept a single argument: 'item', which
         is the item being added to the container.
+
+        Permission -- 'Manage Transient Object Container'
         """
 
     def getDelNotificationTarget(self):
         """
         Returns the current 'before destruction' function, or None.
+
+        Permission -- 'View management screens'
         """
 
     def setDelNotificationTarget(self, f):
@@ -317,4 +197,140 @@ class TransientItemContainer(Interface.Base):
 
         'before destruction' functions need accept a single argument: 'item',
         which is the item being destroyed.
+
+        Permission -- 'Manage Transient Object Container'
         """
+
+
+class TransientObject(Interface.Base):
+    """
+    A transient object is a temporary object contained in a transient
+    object container.
+
+    Most of the time you'll simply treat a transient object as a
+    dictionary. You can use Python sub-item notation::
+
+      SESSION['foo']=1
+      foo=SESSION['foo']
+      del SESSION['foo']
+
+    When using a transient object from Python-based Scripts or DTML
+    you can use the 'get', 'set', and 'delete' methods instead.
+
+    It's important to reassign mutuable sub-items when you change
+    them. For example::
+
+      l=SESSION['myList']
+      l.append('spam')
+      SESSION['myList']=l
+
+    This is necessary in order to save your changes.
+    """
+
+    def getId(self):
+        """
+        Returns a meaningful unique id for the object.
+
+        Permission -- Always available
+        """
+        
+    def invalidate(self):
+        """
+        Invalidate (expire) the transient object.
+
+        Causes the transient object container's "before destruct" method
+        related to this object to be called as a side effect.
+
+        Permission -- XXX        
+        """
+
+    def getLastAccessed(self):
+        """
+        Return the time the transient object was last accessed in
+        integer seconds-since-the-epoch form.
+
+        Permission -- XXX        
+        """
+
+    def setLastAccessed(self):
+        """
+        Cause the last accessed time to be set to now.
+
+        Permission -- XXX      
+        """
+
+    def getCreated(self):
+        """
+        Return the time the transient object was created in integer
+        seconds-since-the-epoch form.
+
+        Permission -- XXX        
+        """
+
+    def keys(self):
+        """
+        Return sequence of key elements.
+
+        Permission -- XXX        
+        """
+
+    def values(self):
+        """
+        Return sequence of value elements.
+
+        Permission -- XXX        
+        """
+
+    def items(self):
+        """
+        Return sequence of (key, value) elements.
+
+        Permission -- XXX        
+        """
+
+    def get(self, k, default='marker'):
+        """
+        Return value associated with key k.  If k does not exist and default
+        is not marker, return default, else raise KeyError.
+
+        Permission -- XXX
+        """
+
+    def has_key(self, k):
+        """
+        Return true if item referenced by key k exists.
+
+        Permission -- XXX
+        """
+
+    def clear(self):
+        """
+        Remove all key/value pairs.
+
+        Permission -- XXX
+        """
+
+    def update(self, d):
+        """
+        Merge dictionary d into ourselves.
+
+        Permission -- XXX
+        """
+
+    def set(self, k, v):
+        """
+        Call __setitem__ with key k, value v.
+
+        Permission -- XXX
+        """
+
+    def delete(self, k):
+        """
+        Call __delitem__ with key k.
+
+        Permission -- XXX
+        """
+
+
+
+
