d4f24029d99d50bcd18e23ced57db4fedfb89e0e
Andreas Jung <yet@gmx.de>
2002-01-02 16:27:24 +0000

    updated

--- lib/python/Products/PluginIndexes/TextIndex/Splitter/setup.py
+++ lib/python/Products/PluginIndexes/TextIndex/Splitter/setup.py
@@ -10,15 +10,14 @@ LIBS=[]
 
 
 setup (name = "Splitter",
-       version = "1.0",
-       description = "Splitters for Zope 2.4",
-       author = "Andreas Jung",
-       author_email = "andreas@zope.com",
-       url = "http://www.zope.org/...",        
-       ext_modules=[Extension("ZopeSplitter",['ZopeSplitter/src/ZopeSplitter.c']), \
-                    Extension("ISO_8859_1_Splitter",['ISO_8859_1_Splitter/src/ISO_8859_1_Splitter.c']),   \
-                    Extension("UnicodeSplitter",['UnicodeSplitter/src/UnicodeSplitter.c'])   \
-                   ]
- 
-
+    version = "1.0",
+    description = "Splitters for Zope 2.5",
+    author = "Andreas Jung",
+    author_email = "andreas@zope.com",
+    url = "http://www.zope.org/...",        
+    ext_modules=[
+        Extension("ZopeSplitter",['ZopeSplitter/src/ZopeSplitter.c']), \
+        Extension("ISO_8859_1_Splitter",['ISO_8859_1_Splitter/src/ISO_8859_1_Splitter.c']),   \
+        Extension("UnicodeSplitter",['UnicodeSplitter/src/UnicodeSplitter.c'])   \
+        ]
       )
