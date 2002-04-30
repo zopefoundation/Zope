942368067c1609d1947dccb533dd276a81678562
Jeremy Hylton <jeremy@svn.zope.org>
2002-04-30 04:33:52 +0000

    Reformat text.

--- lib/python/Products/PluginIndexes/common/PluggableIndex.py
+++ lib/python/Products/PluginIndexes/common/PluggableIndex.py
@@ -12,61 +12,59 @@
 ##############################################################################
 
 """Pluggable Index Base Class """
-__version__='$Revision: 1.5 $'[11:-2]
+__version__='$Revision: 1.6 $'[11:-2]
 
 import Interface
 
 class PluggableIndexInterface(Interface.Base):
 
-    """interface for plugabble indexes"""
-
     def getId():
-        """ return Id of index """
+        """Return Id of index."""
 
     def getEntryForObject(documentId, default=None):
-        """Get all information contained for a specific object by documentId"""
+        """Get all information contained for 'documentId'."""
 
     def index_object(documentId, obj, threshold=None):
-        """Index an object:
+        """Index an object.
 
-           'documentId' is the integer ID of the document
-
-           'obj' is the object to be indexed
-
-           'threshold' is the number of words to process between committing
-           subtransactions.  If None, subtransactions are disabled
+        'documentId' is the integer ID of the document.
+        'obj' is the object to be indexed.
+        'threshold' is the number of words to process between committing
+        subtransactions.  If None, subtransactions are disabled.
         """
 
     def unindex_object(documentId):
-        """Remove the documentId from the index"""
+        """Remove the documentId from the index."""
 
+    # XXX TextIndex does not implement uniqueValues().
     def uniqueValues(name=None, withLengths=0):
         """Returns the unique values for name.
 
-           If 'withLengths' is true, returns a sequence of tuples of
-           (value, length)
+        If 'withLengths' is true, returns a sequence of tuples of
+        (value, length).
         """
-
+        
     def _apply_index(request, cid=''):
-        """Apply the index to query parameters given in the argument, request.
+        """Apply the index to query parameters given in 'request'.
 
-           The argument should be a mapping object.
+        The argument should be a mapping object.
 
-           If the request does not contain the needed parametrs, then None is
-           returned.
+        If the request does not contain the needed parametrs, then
+        None is returned.
 
-           If the request contains a parameter with the name of the column
-           + "_usage", it is sniffed for information on how to handle applying
-           the index. (Note: this style or parameters is deprecated)
+        If the request contains a parameter with the name of the column
+        + "_usage", it is sniffed for information on how to handle applying
+        the index. (Note: this style or parameters is deprecated)
 
-           If the request contains a parameter with the name of the column
-           and this parameter is either a Record or a class instance then
-           it is assumed that the parameters of this index are passed as
-           attribute (Note: this is the recommended way to pass parameters
-           since Zope 2.4)
+        If the request contains a parameter with the name of the
+        column and this parameter is either a Record or a class
+        instance then it is assumed that the parameters of this index
+        are passed as attribute (Note: this is the recommended way to
+        pass parameters since Zope 2.4)
 
-           Otherwise two objects are returned.  The first object is a ResultSet
-           containing the record numbers of the matching records.  The second
-           object is a tuple containing the names of all data fields used.
+        Otherwise two objects are returned.  The first object is a
+        ResultSet containing the record numbers of the matching
+        records.  The second object is a tuple containing the names of
+        all data fields used.
         """
 
