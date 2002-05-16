d9c0428ff734232fe072edf0d2b33f1799a4128f
Jeremy Hylton <jeremy@svn.zope.org>
2002-05-16 23:18:11 +0000

    Produce HTML output instead of text output

--- lib/python/Products/ZCTextIndex/tests/queryhtml.py
+++ lib/python/Products/ZCTextIndex/tests/queryhtml.py
@@ -14,7 +14,18 @@ QUERIES = ["nested recursive functions",
            "articulate information",
            "import default files",
            "gopher ftp http",
+           "documentation",
            ]
+
+def path2url(p):
+    # convert the paths to a python.org URL
+    # hack: only works for the way Jeremy indexed his copy of python.org
+    marker = "www.python.org/."
+    i = p.find(marker)
+    if i == -1:
+        return p
+    i += len(marker)
+    return "http://www.python.org" + p[i:]
 
 def main(rt):
     index = rt["index"]
@@ -28,21 +39,29 @@ def main(rt):
                 t0 = clock()
                 results, num = index.query(query)
                 t1 = clock()
-                print num, query, t1 - t0
+                print "<p>Query: \"%s\"" % query
+                print "<br>Num results: %d" % num
+                print "<br>time.clock(): %s" % (t1 - t0)
                 key = query
                 if i == 0:
+                    print "<ol>"
                     for docid, score in results:
-                        print score, files[docid]
+                        url = path2url(files[docid])
+                        fmt = '<li><a href="%s">%s</A> score = %s'
+                        print fmt % (url, url, score)
+                    print "</ol>"
                     continue
                 l = times.setdefault(key, [])
                 l.append(t1 - t0)
 
     l = times.keys()
     l.sort()
-    print
+    print "<hr>"
     for k in l:
         v = times[k]
-        print min(v), k, " ".join(map(str, v))
+        print "<p>Query: \"%s\"" % k
+        print "<br>Min time: %s" % min(v)
+        print "<br>All times: %s" % " ".join(map(str, v))
 
 if __name__ == "__main__":
     import sys
@@ -65,7 +84,7 @@ if __name__ == "__main__":
             FSPATH = v
 
     fs = FileStorage(FSPATH, read_only=1)
-    db = ZODB.DB(fs)
+    db = ZODB.DB(fs, cache_size=10000)
     cn = db.open()
     rt = cn.root()
     main(rt)
