9018f5a56b486547e287e4629bcc278e9c7b5181
Shane Hathaway <shane@hathawaymix.org>
2002-09-12 15:48:39 +0000

    Merge from 2_5 branch.  Removed dependency on Data.fs.in in unit tests.

--- lib/python/Testing/custom_zodb.py
+++ lib/python/Testing/custom_zodb.py
@@ -1,7 +1,4 @@
-import ZODB, os
-from ZODB.FileStorage import FileStorage
+import ZODB
 from ZODB.DemoStorage import DemoStorage
 
-dfi = os.path.join(SOFTWARE_HOME, '..', '..', 'var', 'Data.fs.in')
-dfi = os.path.abspath(dfi)
-Storage = DemoStorage(base=FileStorage(dfi, read_only=1), quota=(1<<20))
+Storage = DemoStorage(quota=(1<<20))
