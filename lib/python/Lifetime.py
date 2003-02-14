ea6082c630a73e5d13622e602bfd0f41b3634dc5
Fred Drake <fdrake@acm.org>
2003-02-14 05:50:21 +0000

    Oops; not on the trunk!  The previous change was only intended to be on the new-install-branch, which this file is not part of.  Sorry.

--- lib/python/Lifetime.py
+++ lib/python/Lifetime.py
@@ -28,8 +28,7 @@ def shutdown(exit_code,fast = 0):
     global _shutdown_timeout
     if _shutdown_phase == 0:
         # Thread safety? proably no need to care
-        import ZServer
-        ZServer.exit_code = exit_code
+        sys.ZServerExitCode = exit_code
         _shutdown_phase = 1
     if fast:
         # Someone wants us to shutdown fast. This is hooked into SIGTERM - so possibly
