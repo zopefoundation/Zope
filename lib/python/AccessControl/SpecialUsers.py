8bb0d8ee2cfd4ba3cf445d11b1caceb48328ce9d
Fred Drake <fdrake@acm.org>
2003-07-11 14:21:30 +0000

    - elaborated the docstring with an explanation of where the contents   of the module are initialized - added assignments to "document" the names exported by this module,   and, in the case of 'super', squash a warning that gets generated   when using Python 2.3.  (In Python 2.3, setting an attribute of a   module outside of the module's implementation, where the name of the   attribute matches the name of a built-in and the name isn't already   defined in the module, causes a warning to be generated.)

--- lib/python/AccessControl/SpecialUsers.py
+++ lib/python/AccessControl/SpecialUsers.py
@@ -7,11 +7,22 @@
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
+# FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-__doc__='''Place to find special users
+"""Place to find special users
 
-This is needed to avoid a circular import problem.
-$Id: SpecialUsers.py,v 1.4 2002/08/14 21:29:07 mj Exp $'''
-__version__='$Revision: 1.4 $'[11:-2]
+This is needed to avoid a circular import problem.  The 'real' values
+are stored here by the AccessControl.User module as part of it's
+initialization.
+
+$Id: SpecialUsers.py,v 1.5 2003/07/11 14:21:30 fdrake Exp $
+"""
+__version__='$Revision: 1.5 $'[11:-2]
+
+nobody = None
+system = None
+emergency_user = None
+
+# Note: use of the 'super' name is deprecated.
+super = None
