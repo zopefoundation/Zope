f333383999b1cf80f1b703e50a280c0e01b7d45d
Chris McDonough <chrism@plope.com>
2003-07-24 19:58:16 +0000

    Split functionality of mkzopeinstance into mkzopeinstance.py and copyzopeskel.py.  copyzopeskel.py can be used by packagers to create custom skeleton directories.

--- utilities/mkzopeinstance.py
+++ utilities/mkzopeinstance.py
@@ -17,28 +17,25 @@
 usage:  %(program)s [options]
 
 Options:
--d/--dir --  the directory to which the instance files should be installed
 -h/--help -- print this help text
+-d/--dir  -- the dir in which the instance home should be created
 -u/--user NAME:PASSWORD -- set the user name and password of the initial user
--s/--skel -- the 'skeleton' directory which contains instance files
--z/--home -- the zope home directory (aka ZOPE_HOME) (defaults to ..)
--l/--lib --  the zope lib directory (aka SOFTWARE_HOME) (defaults to
-             ../lib/python)
 
-If no arguments are specified, the installer will ask for dir, username, and
-password and will use ZOPEHOME/skel as the skeleton directory.
+When run without arguments, this script will ask for the information necessary
+to create a Zope instance home.
 """
 
 import getopt
 import os
 import shutil
 import sys
+import copyzopeskel
 
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:],
-            "hu:z:d:s:z:l:",
-            ["help", "user=", "dir=", "skel=", "home=", "lib="]
+            "hu:z:d:s:z:l:i:o:g:c:",
+            ["help", "user=", "dir="]
             )
     except getopt.GetoptError, msg:
         usage(sys.stderr, msg)
@@ -47,60 +44,52 @@ def main():
     script = os.path.abspath(sys.argv[0])
     user = None
     password = None
-    dirname = None
-    zopehome = None
-    softwarehome = None
-    skel = None
+    skeltarget = None
 
     for opt, arg in opts:
         if opt in ("-d", "--dir"):
-            dirname = os.path.expanduser(os.path.abspath(arg))
-            if not dirname:
-                usage(sys.stderr, "dirname must not be empty")
+            skeltarget = os.path.abspath(os.path.expanduser(arg))
+            if not skeltarget:
+                usage(sys.stderr, "dir must not be empty")
                 sys.exit(2)
         if opt in ("-h", "--help"):
             usage(sys.stdout)
             sys.exit()
         if opt in ("-u", "--user"):
+            if not arg:
+                usage(sys.stderr, "user must not be empty")
+                sys.exit(2)
             if not ":" in arg:
                 usage(sys.stderr, "user must be specified as name:password")
                 sys.exit(2)
             user, password = arg.split(":", 1)
-        if opt in ("-s", "--skel"):
-            skel = os.path.expanduser(os.path.abspath(arg))
-            if not skel:
-                usage(sys.stderr, "skel must not be empty")
-                sys.exit(2)
-        if opt in ("-h", "--home"):
-            zopehome = os.path.expanduser(os.path.abspath(arg))
-            if not zopehome:
-                usage(sys.stderr, "home must not be empty")
-                sys.exit(2)
-        if opt in ("-l", "--lib"):
-            softwarehome = os.path.expanduser(os.path.abspath(arg))
-            if not softwarehome:
-                usage(sys.stderr, "lib must not be empty")
-                sys.exit(2)
 
-    # interactively ask for dirname and initial user name/passwd
-    if not dirname:
-        dirname = get_dirname()
-    dirname = os.path.expanduser(dirname)
-    inituser = os.path.join(dirname, "inituser")
+    if not skeltarget:
+        # interactively ask for skeltarget and initial user name/passwd.
+        # cant set custom instancehome in interactive mode, we default
+        # to skeltarget.
+        skeltarget = instancehome = os.path.abspath(
+            os.path.expanduser(get_skeltarget())
+            )
+
+    instancehome = skeltarget
+    zopehome = os.path.dirname(os.path.dirname(script))
+    softwarehome = os.path.join(zopehome, "lib", "python")
+    configfile = os.path.join(instancehome, 'etc', 'zope.conf')
+    skelsrc = os.path.join(zopehome, "skel")
+
+    inituser = os.path.join(instancehome, "inituser")
     if not (user or os.path.exists(inituser)):
         user, password = get_inituser()
 
-    # use defaults for zopehome, softwarehome, and skel if they're not
-    # given
-    if zopehome is None:
-        zopehome = os.path.dirname(os.path.dirname(script))
-    if softwarehome is None:
-        softwarehome = os.path.join(zopehome, "lib", "python")
-    if skel is None:
-        skel = os.path.join(zopehome, "skel")
-
-    makeinstance(dirname, user, password, inituser, zopehome, softwarehome,
-                 skel)
+    kw = {
+        "PYTHON": sys.executable,
+        "INSTANCE_HOME": instancehome,
+        "SOFTWARE_HOME": softwarehome,
+        "ZOPE_HOME": zopehome,
+        }
+    copyzopeskel.copyskel(skelsrc, skeltarget, None, None, **kw)
+    write_inituser(inituser, user, password)
 
 def usage(stream, msg=None):
     if msg:
@@ -109,19 +98,19 @@ def usage(stream, msg=None):
     program = os.path.basename(sys.argv[0])
     print >>stream, __doc__ % {"program": program}
 
-def get_dirname():
+def get_skeltarget():
     print 'Please choose a directory in which you\'d like to install'
     print 'Zope "instance home" files such as database files, configuration'
     print 'files, etc.'
     print
     while 1:
-        dirname = raw_input("Directory: ").strip()
-        if dirname == '':
+        skeltarget = raw_input("Directory: ").strip()
+        if skeltarget == '':
             print 'You must specify a directory'
             continue
         else:
             break
-    return dirname
+    return skeltarget
 
 def get_inituser():
     import getpass
@@ -142,36 +131,6 @@ def get_inituser():
             print "Password mismatch, please try again..."
     return user, passwd
 
-def makeinstance(dirname, user, password, inituser, zopehome,
-                 softwarehome, skel):
-    # Create the top of the instance:
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-
-    replacements = {
-        "PYTHON": sys.executable,
-        "INSTANCE_HOME": dirname,
-        "SOFTWARE_HOME": softwarehome,
-        "ZOPE_HOME": zopehome,
-        }
-
-    # This is fairly ugly.  The chdir() makes path manipulation in the
-    # walk() callback a little easier (less magical), so we'll live
-    # with it.
-    pwd = os.getcwd()
-    os.chdir(skel)
-    try:
-        try:
-            os.path.walk(os.curdir, copyskel, (dirname, replacements))
-        finally:
-            os.chdir(pwd)
-    except (IOError, OSError), msg:
-        print >>sys.stderr, msg
-        sys.exit(1)
-
-    if user:
-        write_inituser(inituser, user, password)
-
 def write_inituser(fn, user, password):
     import binascii
     import sha
@@ -180,45 +139,6 @@ def write_inituser(fn, user, password):
     fp.write('%s:{SHA}%s\n' % (user, pw))
     fp.close()
     os.chmod(fn, 0644)
-
-CVS = os.path.normcase("CVS")
-
-def copyskel((basedir, replacements), dirname, names):
-    # Don't recurse into CVS directories:
-    for name in names[:]:
-        if os.path.normcase(name) == CVS:
-            names.remove(name)
-        elif os.path.isfile(os.path.join(dirname, name)):
-            # Copy the file:
-            sn, ext = os.path.splitext(name)
-            if os.path.normcase(ext) == ".in":
-                dst = os.path.join(basedir, dirname, sn)
-                if os.path.exists(dst):
-                    continue
-                copyin(os.path.join(dirname, name), dst, replacements)
-            else:
-                src = os.path.join(dirname, name)
-                dst = os.path.join(basedir, src)
-                if os.path.exists(dst):
-                    continue
-                shutil.copyfile(src, dst)
-        else:
-            # Directory:
-            dn = os.path.join(basedir, dirname, name)
-            if not os.path.exists(dn):
-                os.mkdir(dn)
-
-def copyin(src, dst, replacements):
-    ifp = open(src)
-    text = ifp.read()
-    ifp.close()
-    for k in replacements:
-        text = text.replace("<<%s>>" % k, replacements[k])
-    ofp = open(dst, "w")
-    ofp.write(text)
-    ofp.close()
-    shutil.copymode(src, dst)
-
 
 if __name__ == "__main__":
     main()
