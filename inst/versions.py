74ae98ab32a3e0dba9bac76bd76b55bd82377085
Fred Drake <fdrake@acm.org>
2003-12-17 22:37:55 +0000

    Update version info to match configure script.

--- inst/versions.py
+++ inst/versions.py
@@ -1,7 +1,7 @@
-ZOPE_MAJOR_VERSION  = '2.7'
+ZOPE_MAJOR_VERSION  = '2.8'
 ZOPE_MINOR_VERSION  = '0'
 ZOPE_BRANCH_NAME    = '$Name$'[6:] or 'no-branch'
 
 # always start prerelease branches with '0' to avoid upgrade
 # issues in RPMs
-VERSION_RELEASE_TAG = 'b2+'
+VERSION_RELEASE_TAG = 'a0'
