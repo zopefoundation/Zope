a45f41294250b2c5cb1041917164ade126134afc
Fred Drake <fdrake@acm.org>
2004-04-13 14:26:12 +0000

    zLOG is the past; logging is the future.  Configure the right thing.

--- test.py
+++ test.py
@@ -606,25 +606,11 @@ def main(module_filter, test_filter, libdir):
         os.path.walk(os.curdir, remove_stale_bytecode, None)
 
     global pathinit
-    
-    # Get the log.ini file from the current directory instead of possibly
-    # buried in the build directory.  XXX This isn't perfect because if
-    # log.ini specifies a log file, it'll be relative to the build directory.
-    # Hmm...
-    logini = os.path.abspath('log.ini')
+
+    configure_logging()
 
     # Initialize the path and cwd
     pathinit = PathInit(build, libdir)
-
-    # Initialize the logging module.
-    import logging.config
-    logging.basicConfig()
-    level = os.getenv("LOGGING")
-    if level:
-        level = int(level)
-    else:
-        level = logging.CRITICAL
-    logging.root.setLevel(level)
 
     files = find_tests(module_filter)
     files.sort()
@@ -648,6 +634,26 @@ def main(module_filter, test_filter, libdir):
                 track.update()
     else:
         runner(files, test_filter, debug)
+
+
+def configure_logging():
+    """Initialize the logging module."""
+    import logging.config
+
+    # Get the log.ini file from the current directory instead of possibly
+    # buried in the build directory.  XXX This isn't perfect because if
+    # log.ini specifies a log file, it'll be relative to the build directory.
+    # Hmm...
+    logini = os.path.abspath("log.ini")
+
+    if os.path.exists(logini):
+        logging.config.fileConfig(logini)
+    else:
+        logging.basicConfig()
+
+    if os.environ.has_key("LOGGING"):
+        level = int(os.environ["LOGGING"])
+        logging.getLogger().setLevel(level)
 
 
 def process_args(argv=None):
