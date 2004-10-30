288914f3e998471e2e3823f19c86e3a1e18a5fb8
Andreas Jung <yet@gmx.de>
2004-10-30 09:28:04 +0000

    moved docutils path configuration to sitecustomize.py

---
+++ lib/python/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys, os
+dirname = os.path.dirname(__file__)
+sys.path.append(os.path.join(dirname, 'third_party', 'docutils'))
+sys.path.append(os.path.join(dirname, 'third_party', 'docutils', 'extras'))
