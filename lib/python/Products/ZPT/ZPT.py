d9a4d7385403596f4c47175b1c5a3dbf930b3185
Andreas Jung <yet@gmx.de>
2005-12-08 20:36:03 +0000

    quick'n dirty Z3 ZPT integration, we can edit and render....

--- lib/python/Products/ZPT/ZPT.py
+++ lib/python/Products/ZPT/ZPT.py
@@ -21,18 +21,47 @@ from types import StringType
 from Globals import DTMLFile, ImageFile, MessageDialog, package_home, Persistent
 from zLOG import LOG, ERROR, INFO
 from OFS.SimpleItem import SimpleItem
+import AccessControl
 from AccessControl import getSecurityManager
 
 from zope.pagetemplate.pagetemplate import PageTemplate 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
-class ZPT(PageTemplate):
+class ZPT(SimpleItem, PageTemplate):
     "Zope wrapper for Page Template using TAL, TALES, and METAL"
 
     meta_type = 'ZPT'
 
+    manage_options = (
+        {'label':'Edit', 'action':'pt_editForm',
+         'help': ('PageTemplates', 'PageTemplate_Edit.stx')},
+        {'label':'Test', 'action':'ZScriptHTML_tryForm'},
+        ) \
+        + SimpleItem.manage_options \
+        
+    security = AccessControl.ClassSecurityInfo()
+
     def __init__(self, id, text=None, content_type=None):
         self.id = str(id)
+
+    security.declareProtected('View', '__call__')
+    security.declareProtected('View', 'view')
+    def view(self):
+        """view """
+        return self()
+
+    security.declareProtected('Change Page Templates',
+      'pt_editAction', 'pt_setTitle', 'pt_edit',
+      'pt_upload', 'pt_changePrefs')
+    def pt_editAction(self, REQUEST, title, text, content_type, expand):
+        """Change the title and document."""
+
+        print text
+        print content_type
+        self.pt_edit(text, content_type)
+        message= 'done'
+        return self.pt_editForm(manage_tabs_message=message)
+
 
     pt_editForm = PageTemplateFile('www/ptEdit', globals(),
                                    __name__='pt_editForm')
