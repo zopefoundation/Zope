bbab43537c228d9c7bb477c3182a1b84bf5eb43e
Tim Peters <tim.one@comcast.net>
2006-01-11 20:29:07 +0000

    Remove compiled Python files from the tree before packaging. Convert line ends on more kinds of files.

--- inst/WinBuilders/mk/zope.mk
+++ inst/WinBuilders/mk/zope.mk
@@ -26,14 +26,19 @@ SEDSCRIPT="s@<<VERSION>>@$(ZOPEVERSION)@g;s@<<MAKEFILEDIR>>@$(ESCAPED)@g"
 $(BUILD_DIR)/Zope-$(ZOPEVERSION)-win32.exe: $(BUILD_DIR)/lib/python/version.txt
 	$(SED) $(SEDSCRIPT) < "$(MAKEFILEDIR)/etc/zope.iss.in" | unix2dos > "$(BUILD_DIR)/zope.iss"
 
-	# Remove CVS directories from the build tree.
+	# Remove CVS directories and compiled Python files from the build tree.
 	find $(BUILD_DIR) -name CVS -type d -exec $(RMRF) {} \; -prune
+	find $(BUILD_DIR) -name "*.pyc" -o -name "*.pyo" | xargs $(RM)
 
 	# Convert text files to Windows line ends.  unix2dos has the nice
 	# property that it leaves lines with \r\n alone, so it doesn't hurt
 	# to do this on files already converted to Windows convention.
-	find $(BUILD_DIR) -name "*.py" -o -name "*.txt" -o -name "*.bat" | \
-		xargs unix2dos
+	find $(BUILD_DIR) -name "*.py"   | xargs unix2dos
+	find $(BUILD_DIR) -name "*.txt"  | xargs unix2dos
+	find $(BUILD_DIR) -name "*.bat"  | xargs unix2dos
+	find $(BUILD_DIR) -name "*.conf" | xargs unix2dos
+	find $(BUILD_DIR) -name "*.xml"  | xargs unix2dos
+	find $(BUILD_DIR) -name "*.in"   | xargs unix2dos
 
 	# Build the Inno installer.
 	$(CD) "$(BUILD_DIR)";"$(ISS_COMPILER)" /cc "$(WIN_BUILD_DIR)\zope.iss"
