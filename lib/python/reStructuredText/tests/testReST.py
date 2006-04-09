5087bb8c0b09678210b0eb5358aadb0ae92d7378
Stefan H. Holek <stefan@epy.co.at>
2006-04-09 15:37:24 +0000

    Merged 2.8 branch r30721:30722 into 2.9 branch. (Fix for collector #1770 that never made it.)

--- lib/python/reStructuredText/tests/testReST.py
+++ lib/python/reStructuredText/tests/testReST.py
@@ -1,6 +1,8 @@
 
 import unittest
 
+from reStructuredText import HTML
+
 
 class TestReST(unittest.TestCase):
 
@@ -8,8 +10,22 @@ class TestReST(unittest.TestCase):
         # Make sure we can import the rst parser
         from docutils.parsers import rst
 
+    def testWithSingleSubtitle(self):
+        input = '''
+title
+-----
+subtitle
+++++++++
+text
+'''     
+        expected='''<h3 class="title">title</h3>
+<h4 class="subtitle">subtitle</h4>
+<p>text</p>
+'''
+        output = HTML(input)     
+        self.assertEquals(output, expected) 
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     return TestSuite((makeSuite(TestReST),))
-
