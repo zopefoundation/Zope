a5d4d6463508b55ace765d6c3419891c3e8eb86c
Philipp von Weitershausen <philipp@weitershausen.de>
2006-05-22 01:08:55 +0000

    I found out what a previously discussed failure is about and sketched out a plan how to deal with it.

--- lib/python/Products/PageTemplates/tests/testHTMLTests.py
+++ lib/python/Products/PageTemplates/tests/testHTMLTests.py
@@ -125,11 +125,15 @@ class HTMLTests(zope.component.testing.PlacelessSetup, unittest.TestCase):
         self.assert_expected(self.folder.t, 'Loop1.html')
 
     def checkFancyLoop(self):
-        # XXX iterator/first/bla is supposed to work for some reason,
-        # even though iterator.first is a method that needs to be
-        # called (and that won't happen when it appears inside a path
-        # expr).  I wonder how this worked in Zope 2.9.  Need to
-        # investigate.
+        # XXX This checks among others for a feature of the
+        # PathIterator which lets you do something like this in a path
+        # expr: iterator/last/bla.  This would check whether the
+        # current item in the iteration was the last one with a
+        # particular 'bla' attribute value.  I wonder whether anyone
+        # actually needs this? I vote for ripping it out.  We can
+        # provide BBB for a certain deprecation period by enabling a
+        # specialized ITraversable adapter for ZopeIterator that works
+        # similar to the old PathIterator.
         self.assert_expected(self.folder.t, 'Loop2.html')
 
     def checkGlobalsShadowLocals(self):
