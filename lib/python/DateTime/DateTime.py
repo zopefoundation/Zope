3035655ac97db283706dcb97b92b1fcb0f1d4a32
Stefan H. Holek <stefan@epy.co.at>
2006-06-16 15:55:50 +0000

    Made DateTime.DateTime marshallable via XML-RPC. Fixes http://www.zope.org/Collectors/Zope/2109

--- lib/python/DateTime/DateTime.py
+++ lib/python/DateTime/DateTime.py
@@ -1797,6 +1797,14 @@ class DateTime:
         d1 = (( d4 - L) % 365) + L
         return d1/7 + 1
 
+    def encode(self, out):
+        """
+        Encode value for XML-RPC
+        """
+        out.write('<value><dateTime.iso8601>')
+        out.write(self.ISO8601())
+        out.write('</dateTime.iso8601></value>\n')
+
 
 class strftimeFormatter:
 
