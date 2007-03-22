9730698f079fae6aecc0141dc063932ace64b216
Tres Seaver <tseaver@palladion.com>
2007-03-22 15:44:16 +0000

    Issue #2296:  Fix broken import of ZClass products.

--- lib/python/App/Product.py
+++ lib/python/App/Product.py
@@ -45,6 +45,7 @@ import ZClasses, AccessControl.Owned
 from OFS.Folder import Folder
 from HelpSys.HelpSys import ProductHelp
 from AccessControl import Unauthorized
+from AccessControl.Permissions import manage_zclasses
 
 from Factory import Factory
 from Permission import PermissionManager
@@ -60,7 +61,8 @@ class ProductFolder(Folder):
     meta_type ='Product Management'
     icon='p_/ProductFolder_icon'
 
-    all_meta_types={'name': 'Product', 'action': 'manage_addProductForm'},
+    all_meta_types={'name': 'Product', 'action': 'manage_addProductForm',
+                    'permission': manage_zclasses},
     meta_types=all_meta_types
 
     # This prevents subobjects from being owned!
