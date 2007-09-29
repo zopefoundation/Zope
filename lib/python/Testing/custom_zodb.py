04a5c63cc465191768b5b0404869d39871792eb8
Andreas Jung <yet@gmx.de>
2007-09-29 08:48:46 +0000

    Testing/custom_zodb.py: added support use a different storage other than DemoStorage. A dedicated FileStorage can be mount by setting the $TEST_FILESTORAGE environment variable to a custom Data.fs file.  A ZEO server can be configured using the $TEST_ZEO_HOST and $TEST_ZEO_PORT environment variables. This new functionality allows us to use the standard Zope testrunner for writing and running tests against existing Zope installations.

--- lib/python/Testing/custom_zodb.py
+++ lib/python/Testing/custom_zodb.py
@@ -1,4 +1,35 @@
-import ZODB
-from ZODB.DemoStorage import DemoStorage
 
-Storage = DemoStorage(quota=(1<<20))
+import os
+import logging
+import ZODB
+
+LOG = logging.getLogger('Testing')
+
+def getStorage():
+
+    get = os.environ.get
+    # Support for running tests against an existing ZEO storage
+    # ATT: better configuration options (ajung, 17.09.2007)
+
+    if os.environ.has_key('TEST_ZEO_HOST') and os.environ.has_key('TEST_ZEO_PORT'):
+
+        from ZEO.ClientStorage import ClientStorage
+        zeo_host = get('TEST_ZEO_HOST')
+        zeo_port = int(get('TEST_ZEO_PORT'))
+        LOG.info('Using ZEO server (%s:%d)' % (zeo_host, zeo_port))
+        return ClientStorage((zeo_host, zeo_port))
+
+    elif os.environ.has_key('TEST_FILESTORAGE'):
+
+        import ZODB.FileStorage
+        datafs = get('TEST_FILESTORAGE')
+        LOG.info('Using Filestorage at (%s)' % datafs)
+        return ZODB.FileStorage.FileStorage(datafs)
+
+    else:
+        from ZODB.DemoStorage import DemoStorage
+        Storage = DemoStorage(quota=(1<<20))
+        LOG.info('Using DemoStorage')
+        return DemoStorage(quota=(1<<20))
+
+Storage = getStorage()
