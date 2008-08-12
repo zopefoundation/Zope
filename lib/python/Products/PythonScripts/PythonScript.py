3753d1a2366ecb909c00f310afd90d5dfa459479
Andreas Jung <yet@gmx.de>
2008-08-12 12:51:20 +0000

    - Launchpad #257269: 'raise SystemExit' with a PythonScript could shutdown   a complete Zope instance

--- lib/python/Products/PythonScripts/PythonScript.py
+++ lib/python/Products/PythonScripts/PythonScript.py
@@ -322,7 +322,11 @@ class PythonScript(Script, Historical, Cacheable):
         g['__file__'] = getattr(self, '_filepath', None) or self.get_filepath()
         f = new.function(fcode, g, None, fadefs)
 
-        result = f(*args, **kw)
+        try:
+            result = f(*args, **kw)
+        except SystemExit:
+            raise ValueError('SystemExit can not raised with a PythonScript')
+
         if keyset is not None:
             # Store the result in the cache.
             self.ZCacheable_set(result, keywords=keyset)
--- lib/python/Products/PythonScripts/tests/testPythonScript.py
+++ lib/python/Products/PythonScripts/tests/testPythonScript.py
@@ -222,6 +222,11 @@ class TestPythonScriptNoAq(PythonScriptTestBase):
         res = self._newPS("return DateTime('2007/12/10').strftime('%d.%m.%Y')")()
         self.assertEqual(res, '10.12.2007')
 
+    def testRaiseSystemExitLaunchpad257269(self):
+        ps = self._newPS("raise SystemExit")
+        self.assertRaises(ValueError, ps)
+
+
 
 class TestPythonScriptErrors(PythonScriptTestBase):
     
