cd4efecf7f3af0fe35e279436889c76dd5d5843e
Hanno Schlichting <hanno@hannosch.eu>
2008-08-27 14:05:44 +0000

    Remove tests that depend on the presence of the Examples.zexp

--- lib/python/Testing/ZopeTestCase/testBaseTestCase.py
+++ lib/python/Testing/ZopeTestCase/testBaseTestCase.py
@@ -15,9 +15,8 @@
 NOTE: This is *not* an example TestCase. Do not
 use this file as a blueprint for your own tests!
 
-See testPythonScript.py and testShoppingCart.py for
-example test cases. See testSkeleton.py for a quick
-way of getting started.
+See testPythonScript.py for example test cases. See testSkeleton.py for a
+quick way of getting started.
 
 $Id$
 """
