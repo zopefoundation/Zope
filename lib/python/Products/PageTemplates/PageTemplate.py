13ff3f46d3f08549baa15b181853c7d30bdd33df
Andreas Jung <yet@gmx.de>
2008-08-29 10:56:06 +0000

    - Launchpad #262313: respect the 'Expand macros when editing' flag   when editing a page template through the ZMI

--- lib/python/Products/PageTemplates/PageTemplate.py
+++ lib/python/Products/PageTemplates/PageTemplate.py
@@ -74,6 +74,8 @@ class PageTemplate(ExtensionClass.Base,
             sourceAnnotations = getattr(debug, 'sourceAnnotations', False)
         else:
             showtal = sourceAnnotations = False
+        if source:
+            showtal = True
         return super(PageTemplate, self).pt_render(c, source=source, sourceAnnotations=sourceAnnotations,
                    showtal=showtal)
 
