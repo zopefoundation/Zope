2acb087617851b7ad95df546c7e5d8b17282382a
Andreas Jung <yet@gmx.de>
2008-10-22 12:05:51 +0000

    preparing 2.11.2

--- inst/versions.py
+++ inst/versions.py
@@ -1,5 +1,5 @@
 ZOPE_MAJOR_VERSION  = '2.11'
-ZOPE_MINOR_VERSION  = '1'
+ZOPE_MINOR_VERSION  = '2'
 ZOPE_BRANCH_NAME    = '$Name$'[6:] or 'no-branch'
 
 # always start prerelease branches with '0' to avoid upgrade
