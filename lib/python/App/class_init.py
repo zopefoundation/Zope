3dda5f34ddc1e1a8c59e6c5b814dea0e365b73e0
Tres Seaver <tseaver@palladion.com>
2008-12-29 20:06:47 +0000

    Add familiar InitializeClass alias.

--- lib/python/App/class_init.py
+++ lib/python/App/class_init.py
@@ -81,3 +81,5 @@ def default__class_init__(self):
                         "Class %s.%s has a security declaration for "
                         "nonexistent method %r", self.__module__,
                         self.__name__, mname)
+
+InitializeClass = default__class_init__ # instead of Globals.Initialize
