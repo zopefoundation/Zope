552418405c7910fcf2a46c0eb16a027026030df3
Andreas Jung <yet@gmx.de>
2009-02-04 11:04:49 +0000

    - LP/#324876: tighened regex for detecting the charset   from a meta-equiv header

--- lib/python/Products/PageTemplates/utils.py
+++ lib/python/Products/PageTemplates/utils.py
@@ -20,7 +20,7 @@ import re
 
 
 xml_preamble_reg = re.compile(r'^<\?xml.*?encoding="(.*?)".*?\?>', re.M)
-http_equiv_reg = re.compile(r'(<meta.*?http\-equiv.*?content-type.*?>)', re.I|re.M|re.S)
+http_equiv_reg = re.compile(r'(<meta\s+[^>]*?http\-equiv[^>]*?content-type.*?>)', re.I|re.M|re.S)
 http_equiv_reg2 = re.compile(r'charset.*?=.*?(?P<charset>[\w\-]*)', re.I|re.M|re.S)
 
 def encodingFromXMLPreamble(xml):
