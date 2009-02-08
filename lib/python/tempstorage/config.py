3f8b32dff13a6643c71c7a7bdc09381b1a80e4cd
Hanno Schlichting <hanno@hannosch.eu>
2009-02-08 16:34:30 +0000

    Pull in tempstorage from the new package home

--- lib/python/tempstorage/config.py
+++
@@ -1,6 +0,0 @@
-from ZODB.config import BaseConfig
-
-class TemporaryStorage(BaseConfig):
-    def open(self):
-        from tempstorage.TemporaryStorage import TemporaryStorage
-        return TemporaryStorage(self.config.name)
