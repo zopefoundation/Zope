b670bb7e334948b1fc449f84e094eedae995045a
Martijn Pieters <mj@zopatista.co>
2009-09-18 20:55:40 +0000

    Backport r104360 from trunk: do not emit the request closed event from a request clone

--- lib/python/ZPublisher/HTTPRequest.py
+++ lib/python/ZPublisher/HTTPRequest.py
@@ -149,7 +149,7 @@ class HTTPRequest(BaseRequest):
         r.retry_count=self.retry_count
         return r
 
-    def close(self):
+    def clear(self):
         # Clear all references to the input stream, possibly
         # removing tempfiles.
         self.stdin = None
@@ -159,7 +159,7 @@ class HTTPRequest(BaseRequest):
         # one.  Without this, there's the possibility of memory leaking
         # after every request.
         self._lazies = {}
-        BaseRequest.close(self)
+        BaseRequest.clear(self)
 
     def setServerURL(self, protocol=None, hostname=None, port=None):
         """ Set the parts of generated URLs. """
@@ -1082,7 +1082,7 @@ class HTTPRequest(BaseRequest):
         try: object=req.traverse(path)
         except: rsp.exception()
         if object is None:
-            req.close()
+            req.clear()
             raise rsp.errmsg, sys.exc_info()[1]
 
         # The traversal machinery may return a "default object"
@@ -1100,7 +1100,7 @@ class HTTPRequest(BaseRequest):
         if name != os.path.split(path)[-1]:
             object=req.PARENTS[0]
 
-        req.close()
+        req.clear()
         return object
 
 
