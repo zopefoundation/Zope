a5596551168caceec987d1942b800206817285a0
Baiju Muthukadan <baiju.m.mail@gmail.com>
2009-10-13 09:01:15 +0000

    pkg_resources should be always available (setuptools is a hard-dependency for Zope 2)

--- src/Products/__init__.py
+++ src/Products/__init__.py
@@ -11,8 +11,4 @@
 #
 ##############################################################################
 # See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
