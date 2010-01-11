1c1dc58e962312e130bd1a4dc6743b7205479e51
Tres Seaver <tseaver@palladion.com>
2010-01-11 21:57:47 +0000

    Forward port better fix and test for LP #491224 from 2.8 branch.

--- lib/python/OFS/SimpleItem.py
+++ lib/python/OFS/SimpleItem.py
@@ -36,7 +36,6 @@ from DocumentTemplate.html_quote import html_quote
 from DocumentTemplate.ustr import ustr
 from ExtensionClass import Base
 from webdav.Resource import Resource
-from webdav.xmltools import escape as xml_escape
 from zExceptions import Redirect
 from zExceptions.ExceptionFormatter import format_exception
 from zope.interface import implements
@@ -234,7 +233,7 @@ class Item(Base, Resource, CopySource, App.Management.Tabs, Traversable,
                     exc_info=True
                     )
                 try:
-                    strv = str(error_value)
+                    strv = repr(error_value) # quotes tainted strings
                 except:
                     strv = ('<unprintable %s object>' % 
                             str(type(error_value).__name__))
@@ -244,7 +243,6 @@ class Item(Base, Resource, CopySource, App.Management.Tabs, Traversable,
                      "event log for full details: %s)")%(
                     html_quote(sys.exc_info()[1]),
                     ))
-            v = xml_escape(v)
             raise error_type, v, tb
         finally:
             if hasattr(self, '_v_eek'): del self._v_eek
