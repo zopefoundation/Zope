c022bbec7b24f4ace34f1638e138d77702a89f8c
Hanno Schlichting <hanno@hannosch.eu>
2010-06-05 22:33:49 +0000

    There's only one type of interfaces left, no need for a z3 prefix

--- src/AccessControl/interfaces.py
+++ src/AccessControl/interfaces.py
@@ -10,7 +10,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""AccessControl z3 interfaces.
+"""AccessControl interfaces.
 
 $Id$
 """
--- src/AccessControl/tests/testRole.py
+++ src/AccessControl/tests/testRole.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestRoleManager(unittest.TestCase):
 
-    def test_z3interfaces(self):
+    def test_interfaces(self):
         from AccessControl.interfaces import IRoleManager
         from AccessControl.Role import RoleManager
         from zope.interface.verify import verifyClass
@@ -15,6 +15,3 @@ def test_suite():
     return unittest.TestSuite((
         unittest.makeSuite(TestRoleManager),
         ))
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
