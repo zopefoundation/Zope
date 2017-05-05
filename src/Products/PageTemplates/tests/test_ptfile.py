99aab7e38f6d529a15f249b558d8e5563d87ef07
Robert Buchholz <rbu@goodpoint.de>
2017-05-05 08:12:57 +0200

    Port PageTemplateFile to Py3

--- src/Products/PageTemplates/tests/test_ptfile.py
+++ src/Products/PageTemplates/tests/test_ptfile.py
@@ -39,103 +39,102 @@ class TypeSniffingTestCase(unittest.TestCase):
 
     def test_sniffer_xml_ascii(self):
         self.check_content_type(
-            "<?xml version='1.0' encoding='ascii'?><doc/>",
+            b"<?xml version='1.0' encoding='ascii'?><doc/>",
             "text/xml")
         self.check_content_type(
-            "<?xml\tversion='1.0' encoding='ascii'?><doc/>",
+            b"<?xml\tversion='1.0' encoding='ascii'?><doc/>",
             "text/xml")
 
     def test_sniffer_xml_utf8(self):
         # w/out byte order mark
         self.check_content_type(
-            "<?xml version='1.0' encoding='utf-8'?><doc/>",
+            b"<?xml version='1.0' encoding='utf-8'?><doc/>",
             "text/xml")
         self.check_content_type(
-            "<?xml\tversion='1.0' encoding='utf-8'?><doc/>",
+            b"<?xml\tversion='1.0' encoding='utf-8'?><doc/>",
             "text/xml")
         # with byte order mark
         self.check_content_type(
-            "\xef\xbb\xbf<?xml version='1.0' encoding='utf-8'?><doc/>",
+            b"\xef\xbb\xbf<?xml version='1.0' encoding='utf-8'?><doc/>",
             "text/xml")
         self.check_content_type(
-            "\xef\xbb\xbf<?xml\tversion='1.0' encoding='utf-8'?><doc/>",
+            b"\xef\xbb\xbf<?xml\tversion='1.0' encoding='utf-8'?><doc/>",
             "text/xml")
 
     def test_sniffer_xml_utf16_be(self):
         # w/out byte order mark
         self.check_content_type(
-            "\0<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
-            "\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
-            "\0<\0d\0o\0c\0/\0>",
+            b"\0<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
+            b"\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
+            b"\0<\0d\0o\0c\0/\0>",
             "text/xml")
         self.check_content_type(
-            "\0<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
-            "\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
-            "\0<\0d\0o\0c\0/\0>",
+            b"\0<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
+            b"\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
+            b"\0<\0d\0o\0c\0/\0>",
             "text/xml")
         # with byte order mark
         self.check_content_type(
-            "\xfe\xff"
-            "\0<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
-            "\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
-            "\0<\0d\0o\0c\0/\0>",
+            b"\xfe\xff"
+            b"\0<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
+            b"\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
+            b"\0<\0d\0o\0c\0/\0>",
             "text/xml")
         self.check_content_type(
-            "\xfe\xff"
-            "\0<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
-            "\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
-            "\0<\0d\0o\0c\0/\0>",
+            b"\xfe\xff"
+            b"\0<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'"
+            b"\0 \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>"
+            b"\0<\0d\0o\0c\0/\0>",
             "text/xml")
 
     def test_sniffer_xml_utf16_le(self):
         # w/out byte order mark
         self.check_content_type(
-            "<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
-            " \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
-            "<\0d\0o\0c\0/\0>\n",
+            b"<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
+            b" \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
+            b"<\0d\0o\0c\0/\0>\n",
             "text/xml")
         self.check_content_type(
-            "<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
-            " \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
-            "<\0d\0o\0c\0/\0>\0",
+            b"<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
+            b" \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
+            b"<\0d\0o\0c\0/\0>\0",
             "text/xml")
         # with byte order mark
         self.check_content_type(
-            "\xff\xfe"
-            "<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
-            " \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
-            "<\0d\0o\0c\0/\0>\0",
+            b"\xff\xfe"
+            b"<\0?\0x\0m\0l\0 \0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
+            b" \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
+            b"<\0d\0o\0c\0/\0>\0",
             "text/xml")
         self.check_content_type(
-            "\xff\xfe"
-            "<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
-            " \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
-            "<\0d\0o\0c\0/\0>\0",
+            b"\xff\xfe"
+            b"<\0?\0x\0m\0l\0\t\0v\0e\0r\0s\0i\0o\0n\0=\0'\01\0.\0000\0'\0"
+            b" \0e\0n\0c\0o\0d\0i\0n\0g\0=\0'\0u\0t\0f\0-\08\0'\0?\0>\0"
+            b"<\0d\0o\0c\0/\0>\0",
             "text/xml")
 
-    HTML_PUBLIC_ID = "-//W3C//DTD HTML 4.01 Transitional//EN"
-    HTML_SYSTEM_ID = "http://www.w3.org/TR/html4/loose.dtd"
+    HTML_PUBLIC_ID = b"-//W3C//DTD HTML 4.01 Transitional//EN"
+    HTML_SYSTEM_ID = b"http://www.w3.org/TR/html4/loose.dtd"
 
     def test_sniffer_html_ascii(self):
         self.check_content_type(
-            "<!DOCTYPE html [ SYSTEM '%s' ]><html></html>"
-            % self.HTML_SYSTEM_ID,
+            b"<!DOCTYPE html [ SYSTEM '" + self.HTML_SYSTEM_ID + b"' ]><html></html>",
             "text/html")
         self.check_content_type(
-            "<html><head><title>sample document</title></head></html>",
+            b"<html><head><title>sample document</title></head></html>",
             "text/html")
 
     # XXX This reflects a case that simply isn't handled by the
     # sniffer; there are many, but it gets it right more often than
     # before.
     def donttest_sniffer_xml_simple(self):
-        self.check_content_type("<doc><element/></doc>",
+        self.check_content_type(b"<doc><element/></doc>",
                                 "text/xml")
 
     def test_getId(self):
         desired_id = os.path.splitext(os.path.split(self.TEMPFILENAME)[-1])[0]
-        f = open(self.TEMPFILENAME, 'w')
-        f.write('Boring')
+        f = open(self.TEMPFILENAME, 'wb')
+        f.write(b'Boring')
         f.close()
         pt = PageTemplateFile(self.TEMPFILENAME)
         pt_id = pt.getId()
@@ -147,8 +146,8 @@ class TypeSniffingTestCase(unittest.TestCase):
     def test_getPhysicalPath(self):
         desired_id = os.path.splitext(os.path.split(self.TEMPFILENAME)[-1])[0]
         desired_path = (desired_id,)
-        f = open(self.TEMPFILENAME, 'w')
-        f.write('Boring')
+        f = open(self.TEMPFILENAME, 'wb')
+        f.write(b'Boring')
         f.close()
         pt = PageTemplateFile(self.TEMPFILENAME)
         pt_path = pt.getPhysicalPath()
@@ -163,10 +162,10 @@ class TypeSniffingTestCase(unittest.TestCase):
 class LineEndingsTestCase(unittest.TestCase):
 
     TEMPFILENAME = tempfile.mktemp(".zpt")
-    TAL = ('''<html tal:replace="python: ' '.join(('foo',''',
-           '''                                    'bar',''',
-           '''                                    'spam',''',
-           '''                                    'eggs'))"></html>''')
+    TAL = (b'''<html tal:replace="python: ' '.join(('foo',''',
+           b'''                                    'bar',''',
+           b'''                                    'spam',''',
+           b'''                                    'eggs'))"></html>''')
     OUTPUT = 'foo bar spam eggs'
 
     def setUp(self):
@@ -179,7 +178,7 @@ class LineEndingsTestCase(unittest.TestCase):
         transaction.abort()
         self.root._p_jar.close()
 
-    def runPTWithLineEndings(self, lineendings='\n'):
+    def runPTWithLineEndings(self, lineendings=b'\n'):
         text = lineendings.join(self.TAL)
         f = open(self.TEMPFILENAME, "wb")
         f.write(text)
@@ -206,8 +205,8 @@ class LazyLoadingTestCase(unittest.TestCase):
             os.unlink(self.TEMPFILENAME)
 
     def test_lazy(self):
-        f = open(self.TEMPFILENAME, 'w')
-        f.write('Lazyness')
+        f = open(self.TEMPFILENAME, 'wb')
+        f.write(b'Laziness')
         f.close()
         pt = PageTemplateFile(self.TEMPFILENAME)
         self.assertTrue(not pt._text and not pt._v_program)
