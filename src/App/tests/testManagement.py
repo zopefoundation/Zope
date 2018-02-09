bca09066274408ecb8c2657b543fa2526fe8149f
Michael Howitz <mh@gocept.com>
2018-02-09 16:22:14 +0100

    Add ability to customise the CSS and JS in ZMI via Properties.
    Requested by customer who uses TTW approach.

--- src/App/tests/testManagement.py
+++ src/App/tests/testManagement.py
@@ -1,7 +1,7 @@
-import unittest
+import Testing.ZopeTestCase
 
 
-class TestNavigation(unittest.TestCase):
+class TestNavigation(Testing.ZopeTestCase.ZopeTestCase):
 
     def test_interfaces(self):
         from App.interfaces import INavigation
@@ -9,3 +9,35 @@ class TestNavigation(unittest.TestCase):
         from zope.interface.verify import verifyClass
 
         verifyClass(INavigation, Navigation)
+
+    def test_Management__Navigation__manage_page_header__1(self):
+        """It respects `zmi_additional_css_paths` string property."""
+        self.folder.manage_addProperty(
+            'zmi_additional_css_paths', '/foo/bar.css', 'string')
+        self.assertIn('href="/foo/bar.css"', self.folder.manage_page_header())
+
+    def test_Management__Navigation__manage_page_header__2(self):
+        """It respects `zmi_additional_css_paths` ustring property."""
+        self.folder.manage_addProperty(
+            'zmi_additional_css_paths', '/foo/bar.css', 'ustring')
+        self.assertIn('href="/foo/bar.css"', self.folder.manage_page_header())
+
+    def test_Management__Navigation__manage_page_header__3(self):
+        """It respects `zmi_additional_css_paths` lines property."""
+        self.folder.manage_addProperty(
+            'zmi_additional_css_paths', ['/foo/bar.css', '/baz.css'], 'lines')
+        self.assertIn('href="/foo/bar.css"', self.folder.manage_page_header())
+        self.assertIn('href="/baz.css"', self.folder.manage_page_header())
+
+    def test_Management__Navigation__manage_page_header__4(self):
+        """It respects `zmi_additional_css_paths` ulines property."""
+        self.folder.manage_addProperty(
+            'zmi_additional_css_paths', ['/foo/bar.css', '/baz.css'], 'lines')
+        self.assertIn('href="/foo/bar.css"', self.folder.manage_page_header())
+        self.assertIn('href="/baz.css"', self.folder.manage_page_header())
+
+    def test_Management__Navigation__manage_page_header__5(self):
+        """It ignores an empty `zmi_additional_css_paths` property."""
+        self.folder.manage_addProperty(
+            'zmi_additional_css_paths', '', 'string')
+        self.assertNotIn('href=""', self.folder.manage_page_header())
