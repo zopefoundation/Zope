4a1c369d7f73bfae28dfb2aa3f2fc528f487ab65
Frank Hoffmann <dr.frank.hoffmann@web.de>
2018-05-17 18:06:39 +0200

    added zmi custom style file

--- src/zmi/styles/subscriber.py
+++ src/zmi/styles/subscriber.py
@@ -8,6 +8,7 @@ def css_paths(context):
     return (
         '/++resource++zmi/bootstrap-4.1.1/bootstrap.min.css',
         '/++resource++zmi/zopetello/css/zopetello.css',
+        '/++resource++zmi/zmi_base.css',
     )
 
 
