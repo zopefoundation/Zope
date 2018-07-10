03e5132743efdfe2ad0642c212f9f34bbf5ff974
Martin Häcker <mhaecker@mac.com>
2018-07-10 15:54:49 +0200

    Indentation and linter fixes

--- src/zmi/styles/resources/zmi_base.js
+++ src/zmi/styles/resources/zmi_base.js
@@ -8,7 +8,7 @@ function toggle_menu() {
 		window.parent.location.href="manage_main";
 	} else {
 		window.parent.location.href="manage";
-	};
+	}
 }
 
 // [2] Add New Object Item (with Modal Dialog)
@@ -26,7 +26,7 @@ function addItem( elm, base_url ) {
 		'manage_addErrorLog',
 		'manage_addVirtualHostMonster',
 		'manage_addzmsform'
-	]
+	];
 
 	// SHOW MODAL DIALOG	
 	if ( $.inArray(action, no_modal_dialog) < 0 ) {
@@ -54,8 +54,8 @@ function addItem( elm, base_url ) {
 			//Modify Form Action for Modal Use
 			$( '#zmi-modal form' ).each( function() {
 				var modal_form_url = modal_form_base + $( this ).attr( 'action' );
-				$( this ).attr( 'action', modal_form_url )
-			})
+				$( this ).attr( 'action', modal_form_url );
+			});
 
 			// GUI FIX FOR MODAL DIALOG: Add Minimal Style Patches to Ancient Zope Forms
 			fix_ancient_modal_gui();
@@ -114,154 +114,155 @@ function addItem( elm, base_url ) {
 		"Session Data Manager":{ "title":"Session Data Manager", "class":"far fa-clock" },
 		"Cookie Crumbler":{ "title":"Cookie Crumbler", "class":"fa fa-cookie-bite" },
 		"Broken object":{ "title":"Broken object", "class":"fas fa-ban text-danger" }
-	}
-// PROCESS Object Icons
-		for ( i in zmi_icons ) {
-			var i_name = i;
-			var i_title =zmi_icons[i].title;
-			var i_class =zmi_icons[i].class;
-			if ( $('i[title*="'+i_name+'"]').hasClass('zmi_icon-broken') ) {
-			 	i_class += ' zmi_icon-broken'
-			};
-			$('i[title*="'+i_name+'"]').replaceWith('<i data-title="'+i_title+'" class="'+i_class+'"></i>');
+	};
+	
+	// PROCESS Object Icons
+	for ( var i in zmi_icons ) {
+		var i_name = i;
+		var i_title =zmi_icons[i].title;
+		var i_class =zmi_icons[i].class;
+		if ( $('i[title*="'+i_name+'"]').hasClass('zmi_icon-broken') ) {
+		 	i_class += ' zmi_icon-broken';
 		}
-// PROCESS Other Icons
-		$('i[title*="/p_/pl"]').replaceWith('<i data-title="Expand..." class="far fa-plus-square"></i>');
-		$('i[title*="/p_/mi"]').replaceWith('<i data-title="Collapse..." class="far fa-minus-square"></i>');
-		$('i[title*="/p_/davlocked"]').replaceWith('<i data-title="WebDAV" class="fa fa-retweet"></i>');
-		$('img[src*="misc_"]').replaceWith('<i class="fa fa-circle-blank"></i>');
-		$('img[src*="zms_"]').replaceWith('<i class="fa fa-circle-blank"></i>');
-		$('#menu_tree td[width="16"] a:contains("+")').html(('<i title="Expand..." class="fas fa-caret-right text-muted"></i>'));
-		$('#menu_tree td[width="16"] a:contains("-")').html(('<i title="Collapse..." class="fas fa-caret-down text-muted"></i>'));
-		$('a[href*="HelpSys"]').empty()
-			.append('<i class="fa fa-question-sign"></i>')
-			.css('border-color','transparent');
+		$('i[title*="'+i_name+'"]').replaceWith('<i data-title="'+i_title+'" class="'+i_class+'"></i>');
 	}
+	// PROCESS Other Icons
+	$('i[title*="/p_/pl"]').replaceWith('<i data-title="Expand..." class="far fa-plus-square"></i>');
+	$('i[title*="/p_/mi"]').replaceWith('<i data-title="Collapse..." class="far fa-minus-square"></i>');
+	$('i[title*="/p_/davlocked"]').replaceWith('<i data-title="WebDAV" class="fa fa-retweet"></i>');
+	$('img[src*="misc_"]').replaceWith('<i class="fa fa-circle-blank"></i>');
+	$('img[src*="zms_"]').replaceWith('<i class="fa fa-circle-blank"></i>');
+	$('#menu_tree td[width="16"] a:contains("+")').html(('<i title="Expand..." class="fas fa-caret-right text-muted"></i>'));
+	$('#menu_tree td[width="16"] a:contains("-")').html(('<i title="Collapse..." class="fas fa-caret-down text-muted"></i>'));
+	$('a[href*="HelpSys"]').empty()
+		.append('<i class="fa fa-question-sign"></i>')
+		.css('border-color','transparent');
+}
 
 // [2] GUI FIX: Add Minimal Style Patches to Ancient Zope Forms
-	function fix_ancient_gui() {
-// WRAP FORM ELEMENT with fluid-container (if missing)
-		if ( $('main').length==0 ) {
-			$('body>form,body>table,body>h2,body>p').wrapAll('<main class="container-fluid zmi-patch"></main>');
+function fix_ancient_gui() {
+	// WRAP FORM ELEMENT with fluid-container (if missing)
+	if ( 0 === $('main').length ) {
+		$('body>form,body>table,body>h2,body>p').wrapAll('<main class="container-fluid zmi-patch"></main>');
 		// ADD BOOTSTRAP CLASSES
-			$('input[type="text"], input[type="file"], textarea, select').addClass('form-control zmi-patch');
-			$('input[type="submit"]').addClass('btn btn-primary zmi-patch');
-			$('textarea[name*=":text"]').addClass('zmi-code');
-			$('table').addClass('table zmi-patch');
-		}
+		$('input[type="text"], input[type="file"], textarea, select').addClass('form-control zmi-patch');
+		$('input[type="submit"]').addClass('btn btn-primary zmi-patch');
+		$('textarea[name*=":text"]').addClass('zmi-code');
+		$('table').addClass('table zmi-patch');
 	}
+}
 // [3] GUI FIX FOR MODAL DIALOG: Add Minimal Style Patches to Ancient Zope Forms
-	function fix_ancient_modal_gui() {
-		if ( $('.modal-body main').length==0 ) {
-			$('.modal-body>form,.modal-body>table,.modal-body>h2,.modal-body>p,.modal-body>i.zmi-help-icon').wrapAll('<main class="container-fluid zmi-patch"></main>');
+function fix_ancient_modal_gui() {
+	if ( 0 === $('.modal-body main').length ) {
+		$('.modal-body>form,.modal-body>table,.modal-body>h2,.modal-body>p,.modal-body>i.zmi-help-icon').wrapAll('<main class="container-fluid zmi-patch"></main>');
 		// ADD BOOTSTRAP CLASSES
-			$('.modal-body input[type="text"],.modal-body input[type="file"],.modal-body textarea,.modal-body select').addClass('form-control zmi-patch');
-			$('.modal-body input[type="submit"]').addClass('btn btn-primary zmi-patch');
-			$('.modal-body textarea[name*=":text"]').addClass('zmi-code');
-			$('.modal-body table').addClass('table zmi-patch');
-		}
+		$('.modal-body input[type="text"],.modal-body input[type="file"],.modal-body textarea,.modal-body select').addClass('form-control zmi-patch');
+		$('.modal-body input[type="submit"]').addClass('btn btn-primary zmi-patch');
+		$('.modal-body textarea[name*=":text"]').addClass('zmi-code');
+		$('.modal-body table').addClass('table zmi-patch');
 	}
+}
 
-	function show_ace_editor() {
-		// +++++++++++++++++++++++++++++++
-		// +++ Ajax.org Cloud9 Editor
-		// +++ http://ace.ajax.org
-		// +++++++++++++++++++++++++++++++
-		$('#content').wrap('<div id="editor_container" class="form-group"></div>');
-		$('#content').before('<div id="editor">ace editor text</div>');
-		var dom = require("ace/lib/dom");
-		// add command to all new editor instances
-		require("ace/commands/default_commands").commands.push({
-			name: "Toggle Fullscreen",
-			bindKey: "F10",
-			exec: function(editor) {
-				var fullScreen = dom.toggleCssClass(document.body, "fullScreen")
-				dom.setCssClass(editor.container, "fullScreen", fullScreen)
-				editor.setAutoScrollEditorIntoView(!fullScreen)
-				editor.resize()
-			}
-		});
-		// @see https://github.com/ajaxorg/ace/wiki/Embedding---API
-		$("textarea#content").hide();
-		editor = ace.edit("editor");
-		var value = $("textarea#content").val();
-		var content_type = $("input#contenttype").val();
-		if (typeof content_type == "undefined" || content_type == null || content_type == '' || content_type == 'text/x-unknown-content-type') {
-			var absolute_url = $("span#absolute_url").text();
-			var id = absolute_url.substr(absolute_url.lastIndexOf("/")+1);
-			if (id.endsWith(".css")) {
-				content_type = "text/css";
-			}
-			else if (id.endsWith(".less")) {
-				content_type = "text/less";
-			}
-			else if (id.endsWith(".js")) {
-				content_type = "text/javascript";
-			}
-			else {
-				content_type = "text/html";
-			}
+// +++++++++++++++++++++++++++++++
+// +++ Ajax.org Cloud9 Editor
+// +++ http://ace.ajax.org
+// +++++++++++++++++++++++++++++++
+function show_ace_editor() {
+	$('#content').wrap('<div id="editor_container" class="form-group"></div>');
+	$('#content').before('<div id="editor">ace editor text</div>');
+	var dom = require("ace/lib/dom");
+	// add command to all new editor instances
+	require("ace/commands/default_commands").commands.push({
+		name: "Toggle Fullscreen",
+		bindKey: "F10",
+		exec: function(editor) {
+			var fullScreen = dom.toggleCssClass(document.body, "fullScreen");
+			dom.setCssClass(editor.container, "fullScreen", fullScreen);
+			editor.setAutoScrollEditorIntoView(!fullScreen);
+			editor.resize();
 		}
-		if (value.indexOf("<html") == 0) {
+	});
+	// @see https://github.com/ajaxorg/ace/wiki/Embedding---API
+	$("textarea#content").hide();
+	editor = ace.edit("editor");
+	var value = $("textarea#content").val();
+	var content_type = $("input#contenttype").val();
+	if (typeof content_type === "undefined" || ! content_type || content_type === 'text/x-unknown-content-type') {
+		var absolute_url = $("span#absolute_url").text();
+		var id = absolute_url.substr(absolute_url.lastIndexOf("/")+1);
+		if (id.endsWith(".css")) {
+			content_type = "text/css";
+		}
+		else if (id.endsWith(".less")) {
+			content_type = "text/less";
+		}
+		else if (id.endsWith(".js")) {
+			content_type = "text/javascript";
+		}
+		else {
 			content_type = "text/html";
 		}
-		if (value.indexOf("<?xml") == 0 || value.indexOf("tal:") >= 0) {
-			content_type = "text/xml";
-		}
-		if (value.indexOf("#!/usr/bin/python") == 0 || value.indexOf("## Script (Python)") == 0 || $('.zmi-pythonscript').length > 0 ) {
-			content_type = "python";
-		}
-		var mode = "text";
-		if (content_type == "text/html") {
-			mode = "html";
-		}
-		else if (content_type == "text/css" || content_type == "application/css" || content_type == "application/x-css") {
-			mode = "css";
-		}
-		else if (content_type == "text/less") {
-			mode = "less";
-		}
-		else if (content_type == "text/javascript" || content_type == "application/javascript" || content_type == "application/x-javascript") {
-			mode = "javascript";
-		}
-		else if (content_type == "text/xml") {
-			mode = "xml";
-		}
-		else if (content_type == "python") {
-			mode = 'python';
-		}
-		editor.setTheme("ace/theme/eclipse");
-		editor.getSession().setMode('ace/mode/'+mode);
-		editor.getSession().setValue(value);
-		editor.getSession().on("change",function() {
-			$("textarea#content").val(editor.getSession().getValue()).change();
-		});
-	};
+	}
+	if ( 0 === value.indexOf("<html")) {
+		content_type = "text/html";
+	}
+	if ( 0 === value.indexOf("<?xml") || value.indexOf("tal:") >= 0) {
+		content_type = "text/xml";
+	}
+	if ( 0 === value.indexOf("#!/usr/bin/python") || 0 === value.indexOf("## Script (Python)") || $('.zmi-pythonscript').length > 0 ) {
+		content_type = "python";
+	}
+	var mode = "text";
+	if (content_type == "text/html") {
+		mode = "html";
+	}
+	else if (content_type == "text/css" || content_type == "application/css" || content_type == "application/x-css") {
+		mode = "css";
+	}
+	else if (content_type == "text/less") {
+		mode = "less";
+	}
+	else if (content_type == "text/javascript" || content_type == "application/javascript" || content_type == "application/x-javascript") {
+		mode = "javascript";
+	}
+	else if (content_type == "text/xml") {
+		mode = "xml";
+	}
+	else if (content_type == "python") {
+		mode = 'python';
+	}
+	editor.setTheme("ace/theme/eclipse");
+	editor.getSession().setMode('ace/mode/'+mode);
+	editor.getSession().setValue(value);
+	editor.getSession().on("change",function() {
+		$("textarea#content").val(editor.getSession().getValue()).change();
+	});
+}
 
 // ++++++++++++++++++++++++++
 // EXECUTE ON DOCUMENT READY
 // ++++++++++++++++++++++++++
 $(function() {
 
-// SHOW ACE EDITOR
+	// SHOW ACE EDITOR
 	if ($('#content').length > 0 ) {
 		show_ace_editor();
 	}
 
-// EXECUTE DESIGN WORKAROUNDS
-// Needed until ALL GUI Forms are Bootstrap Conformant 
+	// EXECUTE DESIGN WORKAROUNDS
+	// Needed until ALL GUI Forms are Bootstrap Conformant 
 	fix_zmi_icons();
 	fix_ancient_gui();
 
-// EXECUTE FUNCTIONAL WORKAROUNDS 
-// [1] Showing some Menu Elements only on List Page as Active
+	// EXECUTE FUNCTIONAL WORKAROUNDS 
+	// [1] Showing some Menu Elements only on List Page as Active
 	if ($('.nav a[href="manage_findForm"]').length > 0 ) {
 		$('#addItemSelect, #toggle_menu').css('opacity',1);
 		$('#addItemSelect').removeAttr('disabled');
 		$('#addItemSelect').attr( 'title', $('#addItemSelect').attr('data-title-active') );
 		$('#toggle_menu').attr( 'title', $('#toggle_menu').attr('data-title-active') );
 	} else {
-		$('#addItemSelect, #toggle_menu').css('opacity',.5);
+		$('#addItemSelect, #toggle_menu').css('opacity', 0.5);
 		$('#addItemSelect').attr('disabled','disabled');
 		$('#addItemSelect').attr( 'title', $('#addItemSelect').attr('data-title-inactive') );
 		$('#toggle_menu').attr( 'title', $('#toggle_menu').attr('data-title-inactive') );
