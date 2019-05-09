5f4192b55702c939f4b9092f1c67922788575fa2
Jens Hinghaus <jh@perfact.de>
2019-05-09 14:30:18 +0200

    Add test for searching in undecodable data

--- src/OFS/tests/testFindSupport.py
+++ src/OFS/tests/testFindSupport.py
@@ -77,6 +77,17 @@ class TestFindSupport(unittest.TestCase):
         self.assertEqual(len(res), 2)
         self.assertEqual(set([x[0] for x in res]), set(['text', 'bytes']))
 
+    @unittest.skipIf(six.PY2, 'Not applicable under Python 2')
+    def test_find_text_nondecodable(self):
+        # Make sure ZopeFind does not crash searching text in nondecodable data
+        encoded = b'\xf6'
+        self.base['bytes'] = DummyItem('bytes', text=encoded)
+
+        try:
+            self.base.ZopeFind(self.base, obj_searchterm='anything')
+        except UnicodeDecodeError:
+            self.fail('ZopeFind in undecodable data raises UnicodeDecodeError')
+
     def test_find_text_tainted(self):
         # Make sure ZopeFind can handle "Tainted" text for searches
         # Tainted strings are created when the publisher sees what appears
