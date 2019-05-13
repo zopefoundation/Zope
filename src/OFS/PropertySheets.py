0ce00845d66485a3cb3e62f8e1f5e655f140be8f
Philip Bauer <bauer@starzel.de>
2019-05-13 10:32:44 +0200

    Fix bug when getting propertysheet. Fixes bug introduced in https://github.com/zopefoundation/Zope/commit/b4495eb403c622300b02071a0904b3ad948cc4fe?diff=unified&w=1#diff-3bb20756c73e7cac2fb14728c89e578aR472

--- src/OFS/PropertySheets.py
+++ src/OFS/PropertySheets.py
@@ -470,7 +470,8 @@ class PropertySheets(Traversable, Implicit, Tabs):
     def get(self, name, default=None):
         for propset in self.__propsets__():
             if propset.id == name or \
-               getattr(propset, 'xml_namespace', None) == name:
+               getattr(propset, 'xml_namespace', None) is not None and \
+               propset.xml_namespace() == name:
                 return propset.__of__(self)
         return default
 
