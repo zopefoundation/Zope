zopefoundation/Zope

commit b4495eb403c622300b02071a0904b3ad948cc4fe

- full PEP-8 compliance with the exception of import sorting

src/OFS/ObjectManager.py
@@ -236,8 +236,8 @@ class ObjectManager(
 
         # Look at all globally visible meta types.
         for entry in getattr(Products, 'meta_types', ()):
-            if ((interfaces is not None) or
-                    (entry.get("visibility", None) == "Global")):
+            if interfaces is not None or \
+               entry.get("visibility", None) == "Global":
                 external_candidates.append(entry)
 
         # Filter the list of external candidates based on the
@@ -321,10 +321,10 @@ class ObjectManager(
         # This doesn't try to be more intelligent than _getOb, and doesn't
         # consult _objects (for performance reasons). The common use case
         # is to check that an object does *not* exist.
-        if (id in ('.', '..') or
-                id.startswith('_') or
-                id.startswith('aq_') or
-                id.endswith('__')):
+        if id in ('.', '..') or \
+           id.startswith('_') or \
+           id.startswith('aq_') or \
+           id.endswith('__'):
             return False
         return getattr(aq_base(self), id, None) is not None
 
@@ -512,8 +512,7 @@ class ObjectManager(
                     try:
                         id = i['id']
                         physicalPath = relativePhysicalPath + (id,)
-                        if ((physicalPath not in seen) and
-                                (i['meta_type'] in t)):
+                        if physicalPath not in seen and i['meta_type'] in t:
                             vals.append(get(id))
                             seen[physicalPath] = 1
                     except Exception:
@@ -742,8 +741,8 @@ class ObjectManager(
             if REQUEST.environ.get('FTP_RECURSIVE', 0) == 1:
                 all_files = copy.copy(files)
                 for f in files:
-                    if (hasattr(aq_base(f[1]), 'isPrincipiaFolderish') and
-                            f[1].isPrincipiaFolderish):
+                    if hasattr(aq_base(f[1]), 'isPrincipiaFolderish') and \
+                       f[1].isPrincipiaFolderish:
                         all_files.extend(findChildren(f[1]))
                 files = all_files
 
@@ -755,8 +754,8 @@ class ObjectManager(
 
             files.sort()
 
-            if not (hasattr(self, 'isTopLevelPrincipiaApplicationObject') and
-                    self.isTopLevelPrincipiaApplicationObject):
+            if not hasattr(self, 'isTopLevelPrincipiaApplicationObject') and \
+               self.isTopLevelPrincipiaApplicationObject:
                 files.insert(0, ('..', aq_parent(self)))
             files.insert(0, ('.', self))
             for k, v in files:
@@ -781,8 +780,8 @@ class ObjectManager(
             mode = 0o0040000
             from AccessControl.User import nobody
             # check to see if we are acquiring our objectValues or not
-            if not (len(REQUEST.PARENTS) > 1 and
-                    self.objectValues() == REQUEST.PARENTS[1].objectValues()):
+            if not len(REQUEST.PARENTS) > 1 and \
+               self.objectValues() == REQUEST.PARENTS[1].objectValues():
                 try:
                     if getSecurityManager().validate(
                             None, self, 'manage_FTPlist', self.manage_FTPlist):
@@ -815,8 +814,7 @@ class ObjectManager(
         request = getattr(self, 'REQUEST', None)
         if not (isinstance(request, str) or request is None):
             method = request.get('REQUEST_METHOD', 'GET')
-            if (request.maybe_webdav_client and
-                    method not in ('GET', 'POST')):
+            if request.maybe_webdav_client and method not in ('GET', 'POST'):
                 if bbb.HAS_ZSERVER:
                     from webdav.NullResource import NullResource
                     return NullResource(self, key, request).__of__(self)
@@ -943,8 +941,8 @@ def findChildren(obj, dirname=''):
 
     lst = []
     for name, child in obj.objectItems():
-        if (hasattr(aq_base(child), 'isPrincipiaFolderish') and
-                child.isPrincipiaFolderish):
+        if hasattr(aq_base(child), 'isPrincipiaFolderish') and \
+           child.isPrincipiaFolderish:
             lst.extend(findChildren(child, dirname + obj.id + '/'))
         else:
             lst.append((dirname + obj.id + "/" + name, child))

src/OFS/PropertySheets.py
@@ -469,8 +469,8 @@ class PropertySheets(Traversable, Implicit, Tabs):
     @security.protected(access_contents_information)
     def get(self, name, default=None):
         for propset in self.__propsets__():
-            if propset.id == name or (hasattr(propset, 'xml_namespace') and
-                                      propset.xml_namespace() == name):
+            if propset.id == name or \
+               getattr(propset, 'xml_namespace', None) == name:
                 return propset.__of__(self)
         return default
 

src/OFS/tests/testObjectManager.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from AccessControl.owner import EmergencyUserCannotOwn
 from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SecurityManagement import noSecurityManager
@@ -503,8 +505,7 @@ class ObjectManagerTests(PlacelessSetup, unittest.TestCase):
         # in skel/import. Tolerate both cases.
         self.assertIsInstance(om.list_imports(), list)
         for filename in om.list_imports():
-            self.assertTrue(filename.endswith('.zexp') or
-                            filename.endswith('.xml'))
+            self.assertTrue(os.path.splitext(filename)[1] in ('.zexp', '.xml'))
 
     def test_manage_get_sortedObjects_quote_id(self):
         # manage_get_sortedObjects now returns a urlquoted version
@@ -517,6 +518,7 @@ class ObjectManagerTests(PlacelessSetup, unittest.TestCase):
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]['id'], hash_id)
         self.assertEqual(result[0]['quoted_id'], quote(hash_id))
+
 
 _marker = object()
 

commit 88c853a07d936040970c41b568c83bad102d110b

Fix bug when getting propertysheet and bugs in manage_FTPlist (#624)
* Fix bug when getting propertysheet.
Fixes bug introduced in https://github.com/zopefoundation/Zope/commit/b4495eb403c622300b02071a0904b3ad948cc4fe?diff=unified&w=1#diff-3bb20756c73e7cac2fb14728c89e578aR472

* I don't really know how this was broken but a change from
https://github.com/zopefoundation/Zope/commit/b4495eb403c622300b02071a0904b3ad948cc4fe#diff-f7f065aa241603d275d21b8e191fa844
broke the test plone.app.testing.layers_zserver.rst

* fix another failing test (plone.testing.zserver.rst)

* - make flake8 happy

* fix another bug in manage_FTPlist introduced in https://github.com/zopefoundation/Zope/commit/b4495eb403c622300b02071a0904b3ad948cc4fe#diff-f7f065aa241603d275d21b8e191fa844

* - adding tests that cover the changes

src/OFS/ObjectManager.py
@@ -769,8 +769,8 @@ class ObjectManager(
 
             files.sort()
 
-            if not hasattr(self, 'isTopLevelPrincipiaApplicationObject') and \
-               self.isTopLevelPrincipiaApplicationObject:
+            if not (hasattr(self, 'isTopLevelPrincipiaApplicationObject')
+                    and self.isTopLevelPrincipiaApplicationObject):
                 files.insert(0, ('..', aq_parent(self)))
             files.insert(0, ('.', self))
             for k, v in files:
@@ -795,8 +795,9 @@ class ObjectManager(
             mode = 0o0040000
             from AccessControl.User import nobody
             # check to see if we are acquiring our objectValues or not
-            if not len(REQUEST.PARENTS) > 1 and \
-               self.objectValues() == REQUEST.PARENTS[1].objectValues():
+            parents = REQUEST.PARENTS
+            if not (len(parents) > 1
+                    and self.objectValues() == parents[1].objectValues()):
                 try:
                     if getSecurityManager().validate(
                             None, self, 'manage_FTPlist', self.manage_FTPlist):

src/OFS/PropertySheets.py
@@ -470,7 +470,7 @@ class PropertySheets(Traversable, Implicit, Tabs):
     def get(self, name, default=None):
         for propset in self.__propsets__():
             if propset.id == name or \
-               getattr(propset, 'xml_namespace', None) == name:
+               getattr(propset, 'xml_namespace', object)() == name:
                 return propset.__of__(self)
         return default
 

src/OFS/bbb.py
@@ -44,4 +44,4 @@ class DAVPropertySheetMixin(object):
 
 
 class DAVProperties(object):
-    pass
+    id = 'webdav'

src/OFS/tests/testObjectManager.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import marshal
 import os
 import unittest
 from logging import getLogger
@@ -17,10 +18,12 @@ from AccessControl.User import User  # before SpecialUsers
 from Acquisition import Implicit
 from Acquisition import aq_self
 from App.config import getConfiguration
+from OFS import bbb
 from OFS.interfaces import IItem
 from OFS.metaconfigure import setDeprecatedManageAddDelete
 from OFS.ObjectManager import ObjectManager
 from OFS.SimpleItem import SimpleItem
+from Testing.makerequest import makerequest
 from zExceptions import BadRequest
 from Zope2.App import zcml
 from zope.component.testing import PlacelessSetup
@@ -548,6 +551,34 @@ class ObjectManagerTests(PlacelessSetup, unittest.TestCase):
 
         # Cleanup
         getConfiguration().zmi_bookmarkable_urls = saved_state
+
+    @unittest.skipUnless(bbb.HAS_ZSERVER, 'Test requires ZServer')
+    def test_FTPList(self):
+        from OFS.Folder import Folder
+        om = makerequest(self._makeOne())
+        om.isTopLevelPrincipiaApplicationObject = True
+        om._setObject('sub', Folder('sub'))
+        om.sub._setObject('subsub', Folder('subsub'))
+        req = om.REQUEST
+        req.PARENTS = [om]
+
+        # At the root we only see a single entry for the subfolder
+        data = marshal.loads(om.manage_FTPlist(req))
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0][0], 'sub')
+
+        # In the subfolder, we see an entry for the current folder
+        # and the folder in it
+        data = marshal.loads(om.sub.manage_FTPlist(req))
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0][0], '.')
+        self.assertEqual(data[1][0], 'subsub')
+
+        # In the leaf node we see entries for the parent and grandparent
+        data = marshal.loads(om.sub.subsub.manage_FTPlist(req))
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0][0], '.')
+        self.assertEqual(data[1][0], '..')
 
 
 _marker = object()

src/OFS/tests/testProperties.py
@@ -103,6 +103,31 @@ class TestPropertyManager(unittest.TestCase):
         self.assertEqual(pm.getProperty('test_lines'), (b'uni', b'code'))
 
 
+class TestPropertySheets(unittest.TestCase):
+
+    def _makePropSheet(self, *args, **kw):
+        from OFS.PropertySheets import PropertySheet
+        return PropertySheet(*args, **kw)
+
+    def _makeFolder(self):
+        from OFS.Folder import Folder
+        fldr = Folder('testfolder')
+        return fldr
+
+    def test_get(self):
+        parent = self._makeFolder()
+        pss = parent.propertysheets
+        sheet_w_name = self._makePropSheet(id='test1')
+        sheet_w_xml = self._makePropSheet(id='foobar',
+                                          md={'xmlns': 'test2'})
+        parent.__propsets__ += (sheet_w_name, sheet_w_xml)
+
+        self.assertIsNone(pss.get('unknown'))
+        self.assertEqual(pss.get('unknown', default='moep'), 'moep')
+        self.assertEqual(pss.get('test1').getId(), 'test1')
+        self.assertEqual(pss.get('test2').getId(), 'foobar')
+
+
 class TestPropertySheet(unittest.TestCase):
 
     def _makeOne(self, *args, **kw):