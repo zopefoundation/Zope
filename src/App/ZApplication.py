ab4efb09e2047a0fc23cf3484764f23afa6e7da1
Michael Howitz <mh@gocept.com>
2019-09-10 08:12:18 +0200

    Remove more ZServer only code.

--- src/App/ZApplication.py
+++ src/App/ZApplication.py
@@ -42,32 +42,8 @@ class ZApplicationWrapper(object):
     def __getattr__(self, name):
         return getattr(self._klass, name)
 
-    def __bobo_traverse__(self, REQUEST=None, name=None):
-        # This is only called by ZServer.ZPublisher.Publish,
-        # as the app in the module_info is used directly.
-
-        conn = self._db.open()
-
-        # arrange for the connection to be closed when the request goes away
-        cleanup = Cleanup(conn)
-        REQUEST._hold(cleanup)
-
-        conn.setDebugInfo(REQUEST.environ, REQUEST.other)
-
-        v = conn.root()[self._name]
-
-        if name is not None:
-            if hasattr(v, '__bobo_traverse__'):
-                return v.__bobo_traverse__(REQUEST, name)
-
-            if hasattr(v, name):
-                return getattr(v, name)
-            return v[name]
-
-        return v
-
     def __call__(self, connection=None):
-        # This is only called by ZPublisher.WSGIPublisher,
+        # This is called by ZPublisher.WSGIPublisher,
         # as load_app calls the app object present in the module_info.
 
         db = self._db
@@ -77,15 +53,3 @@ class ZApplicationWrapper(object):
             connection = db.open(connection)
 
         return connection.root()[self._name]
-
-
-class Cleanup(object):
-    def __init__(self, jar):
-        self._jar = jar
-
-    def __del__(self):
-        if self._jar.transaction_manager._txn is not None:
-            # Only abort a transaction, if one exists. Otherwise the
-            # abort creates a new transaction just to abort it.
-            self._jar.transaction_manager.abort()
-        self._jar.close()
