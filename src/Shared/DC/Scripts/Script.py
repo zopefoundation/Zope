374477dee1f036f5d3e45d71049f36c556ef0b08
Jens Vagelpohl <jens@netz.ooo>
2020-02-03 21:59:26 -0600

    - silence DeprecationnWarnings for DocumentTemplate imports

--- src/Shared/DC/Scripts/Script.py
+++ src/Shared/DC/Scripts/Script.py
@@ -21,7 +21,7 @@ from AccessControl.class_init import InitializeClass
 from AccessControl.Permissions import view_management_screens
 from AccessControl.SecurityInfo import ClassSecurityInfo
 from App.special_dtml import DTMLFile
-from DocumentTemplate.DT_Util import TemplateDict
+from DocumentTemplate._DocumentTemplate import TemplateDict
 from OFS.SimpleItem import SimpleItem
 from Shared.DC.Scripts.Bindings import defaultBindings  # NOQA
 from Shared.DC.Scripts.BindingsUI import BindingsUI
