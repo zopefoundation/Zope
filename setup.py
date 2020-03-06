ea3853e6578cb6fbca66b79154b7bd275c0cae86
Michael Howitz <mh@gocept.com>
2020-03-06 15:53:00 +0100

    As 3.9 is in its early alpha stage do not yet declare supporting it.

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
