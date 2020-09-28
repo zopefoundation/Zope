514c373f9d3d5583ced75168cbda5ca5d79e61eb
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-09-28 16:08:25 +0200

    Update year
    modified:   src/ZPublisher/tests/test_http_header_utils.py

--- src/ZPublisher/tests/test_http_header_utils.py
+++ src/ZPublisher/tests/test_http_header_utils.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2017 Zope Foundation and Contributors.
+# Copyright (c) 2020 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
