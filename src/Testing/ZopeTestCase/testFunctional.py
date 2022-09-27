759bd1a0cea469986dfeb4bb1059d6c294ddfb70
Jérome Perrin <jerome@nexedi.com>
2022-09-27 22:28:04 +0900

    Testing: make `publish` utility functions handle PATH_INFO
    Fixes #1058
    
    This applies to `Testing.ZopeTestCase.FunctionalTestCase.publish` and
    `html` from zopedoctest, both were using Zope's WSGI application, but
    were not preparing `PATH_INFO` in environ like a WSGI server is supposed
    to do.
    
    As a result, it was not possible to use these utility with %-encoded
    paths or paths with non-ascii characters.
    
    This fixes the issue by encoding the PATH_INFO and also changes the
    internals a bit not to pass a request, but using the request factory,
    so that the request environ is also set by WSGIPublisher, which does
    its own handling of encoded paths.

--- src/Testing/ZopeTestCase/testFunctional.py
+++ src/Testing/ZopeTestCase/testFunctional.py
@@ -60,6 +60,9 @@ class TestFunctional(ZopeTestCase.FunctionalTestCase):
         # A method changing the title property of an object
         self.folder.addDTMLMethod('change_title', file=CHANGE_TITLE_DTML)
 
+        # A method with a non-ascii path
+        self.folder.addDTMLMethod('täst', file=b'test')
+
     def testPublishFolder(self):
         response = self.publish(self.folder_path)
         self.assertEqual(response.getStatus(), 200)
@@ -69,6 +72,16 @@ class TestFunctional(ZopeTestCase.FunctionalTestCase):
         response = self.publish(self.folder_path + '/index_html')
         self.assertEqual(response.getStatus(), 200)
         self.assertEqual(response.getBody(), b'index')
+
+    def testPublishDocumentNonAscii(self):
+        response = self.publish(self.folder_path + '/täst')
+        self.assertEqual(response.getStatus(), 200)
+        self.assertEqual(response.getBody(), b'test')
+
+    def testPublishDocumentNonAsciiUrlEncoded(self):
+        response = self.publish(self.folder_path + '/t%C3%A4st')
+        self.assertEqual(response.getStatus(), 200)
+        self.assertEqual(response.getBody(), b'test')
 
     def testUnauthorized(self):
         response = self.publish(self.folder_path + '/secret_html')
