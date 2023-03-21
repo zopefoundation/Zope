3dedcfd843dc588359c0a789f46e54b64ab86315
Jens Vagelpohl <jens@plyp.com>
2023-03-21 16:12:27 +0100

    - rename folder to match the name used in the Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -188,8 +188,6 @@ epub_exclude_files = ['search.html']
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-# Path to locale diresctories
-locale_dirs = ['locales/']
-
+# Path to locale directory
+locale_dirs = ['locale/']
 gettext_compact = False
-
