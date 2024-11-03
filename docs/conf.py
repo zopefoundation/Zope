db5cd4969cd86e061093873714d4423419f36efd
Jens Vagelpohl <jens@plyp.com>
2024-11-03 14:48:47 +0100

    - prepare release 5.11.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2009-%s, The Zope developer community' % year
 author = 'The Zope developer community'
 
 # The short X.Y version
-version = '5.11'
+version = '5.11.1'
 # The full version, including alpha/beta/rc tags
-release = '5.11'
+release = '5.11.1'
 
 
 # -- General configuration ---------------------------------------------------
